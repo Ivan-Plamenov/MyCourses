26d797ceca314a6f84c4ff900ff17355895ee196
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-22 11:54:52 +0200

    Add Solution

---
+++ SoftUni/JS_Web_Developer/01_Programming_Basics/01b_First_Steps_In_Coding_Exercise/03_Deposit_Calculator.js
@@ -0,0 +1,11 @@
+function calculatorDeposits(input){
+    let sumDeposit = Number(input[0]);
+    let lenghtDeposit = Number(input[1]);
+    let yearlyRate = Number(input[2]) / 100;
+
+    totalAmount = sumDeposit + lenghtDeposit * (( sumDeposit * yearlyRate)/12)
+    console.log(totalAmount)
+}
+
+calculatorDeposits(["200","3","5.7"])
+calculatorDeposits(["2350", "6", "7"])
