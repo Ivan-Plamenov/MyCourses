38a175860b875929fa9dc47fc0960680b469ab25
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-22 11:43:59 +0200

    Add Solution

---
+++ SoftUni/JS_Web_Developer/01_Programming_Basics/01b_First_Steps_In_Coding_Exercise/02_Radians_to_Degrees.js
@@ -0,0 +1,8 @@
+function radiansConverter(input){
+let radians = Number(input[0]);
+degrees = radians * 180/Math.PI;;
+console.log(degrees)
+}
+
+radiansConverter(["3.1416"])
+radiansConverter(["6.2832"])
