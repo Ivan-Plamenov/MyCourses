80987476bbb0145963ac7a4341c08647ab96e95d
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-22 11:38:50 +0200

    Add solution

---
+++ SoftUni/JS_Web_Developer/01_Programming_Basics/01b_First_Steps_In_Coding_Exercise/01_USD_to_BGN.js
@@ -0,0 +1,9 @@
+function currencyConverter(input) {
+let usd = Number(input[0]);
+let bgn = usd * 1.79549;
+console.log(bgn);
+}
+
+currencyConverter(["22"])
+currencyConverter(["100"])
+currencyConverter(["12.5"])
