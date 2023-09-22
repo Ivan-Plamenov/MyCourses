585408dc050bc87b19c1436ce7270c20c9ac2d5e
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-22 11:27:56 +0200

    Add Solution

---
+++ SoftUni/JS_Web_Developer/01_Programming_Basics/01a_First_Steps_In_Coding_Lab/07_Projects_Creation.js
@@ -0,0 +1,10 @@
+function projectsCreation(input){
+let architectName = input[0];
+let numberProjects = Number (input[1]);
+neededHours = numberProjects * 3;;
+
+console.log(`The architect ${architectName} will need ${neededHours} hours to complete ${numberProjects} project/s.`)
+}
+
+// projectsCreation(["George ", 4 ])
+// projectsCreation(["Sanya ", 9 ])
