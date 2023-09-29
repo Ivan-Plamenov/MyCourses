df724905b208bd3d974154855ce8d04198009b31
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 15:28:57 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/02c_Stacks_Queues_Tuples_and_Sets_Exercise/06_Paint_Colors.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/02c_Stacks_Queues_Tuples_and_Sets_Exercise/06_Paint_Colors.py
@@ -1,42 +1,31 @@
 from collections import deque
 
-strings = deque(input().split())
-primary_colors = ["red", "yellow", "blue"]
-secondary_colors = ["orange", "purple", "green"]
-matched_colors = []
+colors_string = deque(input().split())
+main_colors = ["red", "blue", "yellow"]
+secondary_colors = {'orange' : ['red', 'yellow'],
+                    'purple' : ['red', 'blue'],
+                    'green' : ['blue', 'yellow']}
+collected_colors = []
 
-while strings:
-    first_substring = strings.popleft()
-    if strings:
-        last_substring = strings.pop()
+while colors_string:
+    first_str = colors_string.popleft()
+    last_str = colors_string.pop() if colors_string else ''
+
+    if first_str + last_str in main_colors or first_str + last_str in secondary_colors:
+        collected_colors.append(first_str + last_str)
+    elif last_str + first_str in main_colors or last_str + first_str in secondary_colors:
+        collected_colors.append(last_str + first_str)
     else:
-        last_substring = ""
-    color = first_substring + last_substring
-    if color in primary_colors or color in secondary_colors:
-        matched_colors.append(color)
-        continue
-    color = last_substring + first_substring
-    if color in primary_colors or color in secondary_colors:
-        matched_colors.append(color)
-        continue
-    first_substring = first_substring[:-1]
-    last_substring = last_substring[:-1]
-    if first_substring:
-        strings.insert(len(strings) // 2, first_substring)
-    if last_substring:
-        strings.insert(len(strings) // 2, last_substring)
+        if len(first_str) > 1:
+            colors_string.insert(len(colors_string) // 2, first_str[:-1])
+        if len(last_str) > 1:
+            colors_string.insert(len(colors_string) // 2, last_str[: -1])
 
-result = []
-for i in range(len(matched_colors)):
-    current_color = matched_colors[i]
-    if current_color in primary_colors:
-        result.append(current_color)
-        continue
-    if current_color == "orange" and "yellow" in matched_colors and "red" in matched_colors:
-        result.append(current_color)
-    elif current_color == "purple" and "red" in matched_colors and "blue" in matched_colors:
-        result.append(current_color)
-    elif current_color == "green" and "yellow" in matched_colors and "blue" in matched_colors:
-        result.append(current_color)
-
-print(result)
+for color in collected_colors:
+    if color in secondary_colors:
+        for el in secondary_colors[color]:
+            if el not in collected_colors:
+                collected_colors.remove(color)
+                break
+    
+print(collected_colors)
