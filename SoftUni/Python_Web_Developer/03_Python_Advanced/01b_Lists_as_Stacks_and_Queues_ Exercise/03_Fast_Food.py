3dcb4bc6e604668f298d9ad19d6bf6a3381a4ba8
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 10:26:09 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/01b_Lists_as_Stacks_and_Queues_ Exercise/03_Fast_Food.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/01b_Lists_as_Stacks_and_Queues_ Exercise/03_Fast_Food.py
@@ -1,22 +1,17 @@
 from collections import deque
 
 food_quantity = int(input())
-orders = list(map(int, input().split()))
-print(max(orders))
-orders_queue = deque(orders)
-orders_left = []
+orders_queue = deque([int(x) for x in input().split()])
+print(max(orders_queue))
 
-for order in range(len(orders)):
-    current_order = orders_queue.popleft()
-    if food_quantity >= current_order:
-        food_quantity -= current_order
-    else:
-        orders_left.append(str(current_order))
-        while orders_queue:
-            orders_left.append(str(orders_queue.popleft()))
-        print(f"Orders left:", end=" ")
-        print(*orders_left)
-        break
+while orders_queue and orders_queue[0] <= food_quantity:
+    food_quantity -= orders_queue[0]
+    orders_queue.popleft()
 
-if not orders_left:
-    print("Orders complete")
+if orders_queue:
+    print('Orders left:', end='')
+    while orders_queue:
+        print(f' {orders_queue.popleft()}', end='')
+
+else:
+     print("Orders complete")
