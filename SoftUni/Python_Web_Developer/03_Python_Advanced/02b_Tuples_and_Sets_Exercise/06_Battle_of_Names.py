80a3f47188f33eeae691eb7b6e9edb335311e1c7
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 13:58:33 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/06_Battle_of_Names.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/06_Battle_of_Names.py
@@ -1,20 +1,20 @@
-lines_count = int(input())
-row_count = 0
-odd_numbers = set()
-even_numbers = set()
-for _ in range(lines_count):
-    row_count += 1
-    name = [ord(symbol) for symbol in input()]
-    ascii_value = sum(name)
-    integer_divide = ascii_value // row_count
-    if integer_divide % 2 == 0:
-        even_numbers.add(integer_divide)
+n = int(input())
+
+odd_set = set()
+even_set = set()
+
+for i in range(1, n + 1):
+    name_sum = sum(ord(char) for char in input()) // i
+    if name_sum % 2 == 0:
+        even_set.add(name_sum)
     else:
-        odd_numbers.add(integer_divide)
-if sum(odd_numbers) == sum(even_numbers):
-    result = [str(num) for num in odd_numbers.union(even_numbers)]
-elif sum(odd_numbers) > sum(even_numbers):
-    result = [str(num) for num in odd_numbers.difference(even_numbers)]
+        odd_set.add(name_sum)
+
+if sum(odd_set) == sum(even_set):
+    result = odd_set.union(even_set)
+elif sum(odd_set) > sum(even_set):
+    result = odd_set.difference(even_set)
 else:
-    result = [str(num) for num in odd_numbers.symmetric_difference(even_numbers)]
-print(", ".join(result))
+    result = odd_set.symmetric_difference(even_set)
+
+print(*result, sep=', ')
