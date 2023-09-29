a7fbc0b2474bde49ca18e5aaef6a97504cfecaaa
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 13:49:49 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/05_Longest_Intersection.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/05_Longest_Intersection.py
@@ -1,14 +1,18 @@
-lines_count = int(input())
-intersections = {}
+n = int(input())
+
 longest_intersection = set()
-for _ in range(lines_count):
-    first_range, second_range = input().split("-")
-    first_start, first_end = first_range.split(",")
-    second_start, second_end = second_range.split(",")
-    first_set = set(range(int(first_start), int(first_end) + 1))
-    second_set = set(range(int(second_start), int(second_end) + 1))
-    intersection = first_set.intersection(second_set)
+
+for _ in range(n):
+    first_range, second_range = input().split('-')
+    first_start, first_end = [int(x) for x in first_range.split(',')]
+    second_start, second_end = [int(x) for x in second_range.split(',')]
+
+    first_set = set(range(first_start, first_end + 1))
+    second_set = set(range(second_start,  second_end + 1))
+
+    intersection = first_set & second_set
+
     if len(intersection) > len(longest_intersection):
         longest_intersection = intersection
-longest_intersection_numbers = [str(num) for num in longest_intersection]
-print(f"Longest intersection is [{', '.join(longest_intersection_numbers)}] with length {len(longest_intersection)}")
+
+print(f"Longest intersection is {list(longest_intersection)} with length {len(longest_intersection)}")
