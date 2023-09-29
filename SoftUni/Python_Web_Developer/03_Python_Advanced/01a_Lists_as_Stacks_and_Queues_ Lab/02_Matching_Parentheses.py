ff47bca3c7e5bfd046635a106170ec4e6cb086d3
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 09:09:51 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/01a_Lists_as_Stacks_and_Queues_ Lab/02_Matching_Parentheses.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/01a_Lists_as_Stacks_and_Queues_ Lab/02_Matching_Parentheses.py
@@ -1,8 +1,16 @@
 algebraic_expression = input()
 stack = []
 
-for i in range(len(algebraic_expression)):
-    if algebraic_expression[i] == '(':
-        stack.append(i)
-    elif algebraic_expression[i] == ')':
-        print(algebraic_expression[stack.pop():i+1])
+for index in range(len(algebraic_expression)):
+    if algebraic_expression[index] == '(':
+        stack.append(index)
+    elif algebraic_expression[index] == ')':
+        start_index = stack.pop()
+        last_index = index + 1
+        print(algebraic_expression[start_index:last_index])
+
+
+######################## Task ########################
+# You are given an algebraic expression with parentheses.
+# Scan through the string and extract each set of parentheses.
+# Print the result back on the console.
