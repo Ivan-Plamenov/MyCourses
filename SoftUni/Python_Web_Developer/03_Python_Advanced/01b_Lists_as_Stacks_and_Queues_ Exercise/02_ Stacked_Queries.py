4e90059bda5d66d3bfeba4d3abf95fe671870890
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 10:20:14 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/01b_Lists_as_Stacks_and_Queues_ Exercise/02_ Stacked_Queries.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/01b_Lists_as_Stacks_and_Queues_ Exercise/02_ Stacked_Queries.py
@@ -1,24 +1,28 @@
 n = int(input())
-stack = []
 
-for num in range(n):
+number_stack = []
+
+for _ in range(n):
     query = input().split()
     action = query[0]
+
     if action == "1":
         number = int(query[1])
-        stack.append(number)
-    elif action == "2":
-        if stack:
-            stack.pop()
-    elif action == "3":
-        if stack:
-            print(max(stack))
-    elif action == "4":
-        if stack:
-            print(min(stack))
+        number_stack.append(number)
+    
+    elif number_stack:
+        if action == "2":
+            number_stack.pop()
+    
+        elif action == "3":
+            print(max(number_stack))
+    
+        elif action == "4":
+            print(min(number_stack))
 
 reversed_stack = []
-while stack:
-    reversed_stack.append(str(stack.pop()))
 
-print(", ".join(reversed_stack))
+while number_stack:
+    print(number_stack.pop(), end='')
+    if number_stack:
+        print(', ', end='')
