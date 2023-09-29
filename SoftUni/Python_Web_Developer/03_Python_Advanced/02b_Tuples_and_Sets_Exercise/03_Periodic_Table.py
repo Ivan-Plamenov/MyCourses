84888a5cc1774daa4b4fa543023da64c177c6bfd
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 13:30:25 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/03_Periodic_Table.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/03_Periodic_Table.py
@@ -1,7 +1,10 @@
-lines_count = int(input())
+n = int(input())
+
 unique_elements = set()
-for _ in range(lines_count):
-    current_elements = input().split()
-    for element in current_elements:
-        unique_elements.add(element)
-[print(item) for item in unique_elements]
+
+for _ in range(n):
+    elements = input().split()
+    for el in elements:
+        unique_elements.add(el)
+        
+print(*unique_elements, sep="\n")
