f395d813bc3e48ee1a8d3721f465ce9d9c5574b5
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 15:01:52 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/02c_Stacks_Queues_Tuples_and_Sets_Exercise/03_Milkshakes.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/02c_Stacks_Queues_Tuples_and_Sets_Exercise/03_Milkshakes.py
@@ -1,37 +1,35 @@
 from collections import deque
 
-chocolate = list(map(int, input().split(", ")))
-milk_cups = deque(map(int, input().split(", ")))
-prepared_milkshakes = 0
+chocolates = [int(x) for x in input().split(', ')]
+milk_cups = deque(int(x) for x in input().split(', '))
+milkshakes = 0
 
-while True:
-    if len(chocolate) == 0 or len(milk_cups) == 0 or prepared_milkshakes == 5:
-        break
-    current_chocolate = chocolate.pop()
-    current_milk = milk_cups.popleft()
-    if current_chocolate <= 0 and current_milk <= 0:
+while chocolates and milk_cups and milkshakes < 5:
+    if chocolates[-1] <= 0 and milk_cups[0] <= 0:
+        chocolates.pop()
+        milk_cups.popleft()
         continue
-    if current_chocolate <= 0:
-        milk_cups.appendleft(current_milk)
+    if chocolates[-1] <= 0:
+        chocolates.pop()
         continue
-    if current_milk <= 0:
-        chocolate.append(current_chocolate)
+    if milk_cups[0] <= 0:
+        milk_cups.popleft()
         continue
-    if current_chocolate == current_milk:
-        prepared_milkshakes += 1
+
+    if chocolates[-1] == milk_cups[0]:
+        chocolates.pop()
+        milk_cups.popleft()
+        milkshakes += 1
     else:
-        milk_cups.append(current_milk)
-        chocolate.append(current_chocolate - 5)
+        milk_cups.rotate(-1)
+        chocolates[-1] -= 5
 
-if prepared_milkshakes == 5:
-    print(f"Great! You made all the chocolate milkshakes needed!")
+
+
+if milkshakes == 5:
+    print("Great! You made all the chocolate milkshakes needed!")
 else:
     print("Not enough milkshakes.")
-if chocolate:
-    print(f"Chocolate: {', '.join([str(ch) for ch in chocolate])}")
-else:
-    print("Chocolate: empty")
-if milk_cups:
-    print(f"Milk: {', '.join([str(cup) for cup in milk_cups])}")
-else:
-    print("Milk: empty")
+
+print(f"Chocolate: {', '.join([str(x) for x in chocolates]) if chocolates else 'empty'}")
+print(f"Milk: {', '.join([str(x) for x in milk_cups]) if milk_cups else 'empty'}")
