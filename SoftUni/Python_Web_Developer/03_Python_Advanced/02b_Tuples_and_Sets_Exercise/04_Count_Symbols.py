a20057a9ce92bf84c9ac0958ec9ca1d653b8322b
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 13:37:07 +0300

    Solution Added

--- SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/04_Count_Symbols.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/02b_Tuples_and_Sets_Exercise/04_Count_Symbols.py
@@ -1,9 +1,6 @@
-text = input()
-characters = {}
-for character in text:
-    if character not in characters:
-        characters[character] = 0
-    characters[character] += 1
-for key in sorted(characters):
-    occurrences = characters[key]
-    print(f"{key}: {occurrences} time/s")
+text = tuple(input())
+
+unique_symbols = sorted(set(text))
+
+for char in unique_symbols:
+        print(f"{char}: {text.count(char)} time/s")
