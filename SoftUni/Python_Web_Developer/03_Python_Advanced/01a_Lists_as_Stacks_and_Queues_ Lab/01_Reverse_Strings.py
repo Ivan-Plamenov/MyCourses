371b1bccd1220bdbc6f94fdb42b4993edd42c219
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 09:52:45 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/01a_Lists_as_Stacks_and_Queues_ Lab/01_Reverse_Strings.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/01a_Lists_as_Stacks_and_Queues_ Lab/01_Reverse_Strings.py
@@ -2,10 +2,3 @@ input_string = list(input())
 
 while input_string:
     print(input_string.pop(), end='')
-
-
-######################## Task ########################
-# Write a program that:
-#     • Reads an input string
-#     • Reverses it using a stack
-#     • Prints the result back on the console
--- SoftUni/Python_Web_Developer/03_Python_Advanced/01a_Lists_as_Stacks_and_Queues_ Lab/02_Matching_Parentheses.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/01a_Lists_as_Stacks_and_Queues_ Lab/02_Matching_Parentheses.py
@@ -8,9 +8,3 @@ for index in range(len(algebraic_expression)):
         start_index = stack.pop()
         last_index = index + 1
         print(algebraic_expression[start_index:last_index])
-
-
-######################## Task ########################
-# You are given an algebraic expression with parentheses.
-# Scan through the string and extract each set of parentheses.
-# Print the result back on the console.
