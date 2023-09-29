a9500d8591d0a059a0992895a78204bfff340cab
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-29 13:02:19 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/02a_Tuples_and_Sets_Lab/03_Record_Unique_Names.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/02a_Tuples_and_Sets_Lab/03_Record_Unique_Names.py
@@ -3,6 +3,8 @@ n = int(input())
 unique_names = set()
 
 for _ in range(n):
-    unique_names.add(input())
+    name = input()
+    unique_names.add(name)
 
-print('\n'.join(unique_names))
+for name in unique_names:
+    print(name)
