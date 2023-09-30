4a2c48cc9a8438e1ffd330b1f7ec944baad1db31
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 17:17:46 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/04b_Functions_Advanced_Exercise/04_Numbers_Filter.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/04b_Functions_Advanced_Exercise/04_Numbers_Filter.py
@@ -1,10 +1,8 @@
 def even_odd_filter(**kwargs):
-    result = {}
-
-    for key in kwargs:
-        if key == "odd":
-            result[key] = [x for x in kwargs[key] if x % 2 != 0]
+    for key, numbers in kwargs.items():
+        if key == "even":
+            kwargs[key] = [x for x in numbers if x % 2 == 0]
         else:
-            result[key] = [x for x in kwargs[key] if x % 2 == 0]
-
-    return dict(sorted(result.items(), key = lambda x: -len(x[1])))
+            kwargs[key] = [x for x in numbers if x % 2 != 0]
+    
+    return dict(sorted(kwargs.items(), key=lambda x: -len(x[1])))
