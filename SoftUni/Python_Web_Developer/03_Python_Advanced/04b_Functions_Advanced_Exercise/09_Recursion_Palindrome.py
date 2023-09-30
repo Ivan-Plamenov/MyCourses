3e3aef49d386fbfc5b1dacb64810220c9ae8c36c
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 17:42:09 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/04b_Functions_Advanced_Exercise/09_Recursion_Palindrome.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/04b_Functions_Advanced_Exercise/09_Recursion_Palindrome.py
@@ -1,9 +1,10 @@
-def palindrome(word, idx):
-    if idx == len(word) // 2:
+def palindrome(word, index):
+
+    if index == len(word) // 2:
         return f"{word} is a palindrome"
     
-    if word[idx] == word[-idx - 1]:
-        return palindrome(word, idx + 1)
+    if word[index] == word[-index - 1]:
+        return palindrome(word, index + 1)
     
     else:
         return f"{word} is not a palindrome"
