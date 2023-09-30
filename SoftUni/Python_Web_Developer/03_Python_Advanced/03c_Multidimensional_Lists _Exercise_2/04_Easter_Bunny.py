0edf8c1cd938174a7ef1ffe54baa80a2906dcb05
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 11:46:35 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/03c_Multidimensional_Lists _Exercise_2/04_Easter_Bunny.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/03c_Multidimensional_Lists _Exercise_2/04_Easter_Bunny.py
@@ -1,57 +1,41 @@
-def best_path(b_row, b_col):
-    best_direction = ""
-    best_positions = []
-    max_eggs = 0
-    for path in ["up", "down", "left", "right"]:
-        sum_eggs = 0
-        bunny_positions = []
-        if path == "up":
-            for idx in range(b_row - 1, -1, -1):
-                current_egg = field[idx][b_col]
-                if current_egg == "X":
-                    break
-                sum_eggs += current_egg
-                bunny_positions.append([idx, b_col])
-        elif path == "down":
-            for idx in range(b_row + 1, size):
-                current_egg = field[idx][b_col]
-                if current_egg == "X":
-                    break
-                sum_eggs += current_egg
-                bunny_positions.append([idx, b_col])
-        elif path == "left":
-            for idx in range(b_col -1, -1, -1):
-                current_egg = field[b_row][idx]
-                if current_egg == "X":
-                    break
-                sum_eggs += current_egg
-                bunny_positions.append([b_row, idx])
-        elif path == "right":
-            for idx in range(b_col + 1, size):
-                current_egg = field[b_row][idx]
-                if current_egg == "X":
-                    break
-                sum_eggs += current_egg
-                bunny_positions.append([b_row, idx])
-        if sum_eggs >= max_eggs and len(bunny_positions) > 0:
-            best_direction = path
-            best_positions = bunny_positions
-            max_eggs = sum_eggs
-    return best_direction, best_positions, max_eggs
+n = int(input())
 
+matrix = []
+bunny = []
 
-size = int(input())
-field = []
-bunny_row, bunny_col = 0, 0
+for row in range(n):
+    matrix.append(input().split())
+    for col in range(n):
+        if matrix[row][col] == 'B':
+            bunny = [row, col]
 
-for row in range(size):
-    current_row = [x if x in ["X", "B"] else int(x) for x in input().split()]
-    field.append(current_row)
-    for col in range(size):
-        if current_row[col] == "B":
-            bunny_row, bunny_col = row, col
+possible_moves = {'up' : (-1, 0), 
+                  'down' : (1, 0),
+                  'left' : (0, -1),
+                  'right' : (0, 1)}
+max_eggs = float('-inf')
+max_direction = ''
+max_eggs_matrix = []
 
-direction, positions, collected_eggs = best_path(bunny_row, bunny_col)
-print(direction)
-[print(position) for position in positions]
-print(collected_eggs)
+for direction, move in possible_moves.items():
+    eggs = 0
+    curr_eggs_matrix = [] 
+    row = bunny[0] + move[0]
+    col = bunny[1] + move[1]
+
+    while 0 <= row < n and 0 <= col < n:
+        if matrix[row][col] == 'X':
+            break
+        eggs += int(matrix[row][col])
+        curr_eggs_matrix.append([row, col])
+        row += move[0]
+        col += move[1]
+
+    if eggs > max_eggs and curr_eggs_matrix:
+        max_eggs = eggs
+        max_direction = direction
+        max_eggs_matrix = curr_eggs_matrix
+
+print(max_direction)
+[print(row) for row in max_eggs_matrix]
+print(max_eggs)
