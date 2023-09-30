dca578dc8eef99f4424ede0dc45bb0bb512ae371
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 09:56:56 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/03b_Multidimensional_Lists _Exercise_1/03_2x2_Squares_in_Matrix.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/03b_Multidimensional_Lists _Exercise_1/03_2x2_Squares_in_Matrix.py
@@ -1,14 +1,11 @@
 rows, columns = [int(x) for x in input().split()]
-matrix = []
-square_matrices = 0
+matrix = [[x for x in input().split()]for _ in range(rows)]
 
-for _ in range(rows):
-    matrix.append(input().split())
+count = 0
 
 for row in range(rows - 1):
-    for col in range(columns - 1):
-        submatrix_elements = [matrix[row][col], matrix[row][col + 1], matrix[row + 1][col], matrix[row + 1][col + 1]]
-        if len(set(submatrix_elements)) == 1:
-            square_matrices += 1
+    for col in range (columns - 1):
+        if matrix[row][col] == matrix[row][col + 1] == matrix[row + 1][col] == matrix[row + 1][col + 1]:
+            count += 1
 
-print(square_matrices)
+print(count)
