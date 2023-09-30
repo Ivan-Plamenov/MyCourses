a0ea0768033bd9111ab8f63170d9ed0b829d19d3
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 16:38:28 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/04a_Functions_Advanced_Lab/04_Rectangle.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/04a_Functions_Advanced_Lab/04_Rectangle.py
@@ -1,11 +1,12 @@
 def rectangle(length, width):
+        
+    if not isinstance(length, int) or not isinstance(width, int):
+        return "Enter valid values!"
+    
     def perimeter():
         return (length + width) * 2
 
     def area():
         return length * width
-
-    if not isinstance(length, int) or not isinstance(width, int):
-        return "Enter valid values!"
 
     return f"Rectangle area: {area()}\nRectangle perimeter: {perimeter()}"
