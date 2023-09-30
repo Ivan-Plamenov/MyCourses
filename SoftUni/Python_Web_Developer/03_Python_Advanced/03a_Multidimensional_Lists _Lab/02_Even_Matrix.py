dfca8b6efe062757e748608d559dccf88f1309e7
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 08:50:13 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/03a_Multidimensional_Lists _Lab/02_Even_Matrix.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/03a_Multidimensional_Lists _Lab/02_Even_Matrix.py
@@ -1,8 +1,9 @@
 rows = int(input())
+
 matrix = []
 
-for _ in range(rows):
-    data = [int(num) for num in input().split(", ") if int(num) % 2 == 0]
-    matrix.append(data)
+for row_index in range(rows):
+    elements = [int(el) for el in input().split(", ") if int(el) % 2 == 0]
+    matrix.append(elements)
 
 print(matrix)
