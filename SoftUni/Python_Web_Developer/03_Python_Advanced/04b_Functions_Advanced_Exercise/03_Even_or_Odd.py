8f361575b97317639ca2449bedb4f2c869d32a84
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 17:11:42 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/04b_Functions_Advanced_Exercise/03_Even_or_Odd.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/04b_Functions_Advanced_Exercise/03_Even_or_Odd.py
@@ -1,11 +1,6 @@
 def even_odd(*args):
-    *numbers, command = args
-    result = []
+    command = args[-1]
     
     if command == "even":
-        [result.append(x) for x in numbers if x % 2 == 0]
-    
-    elif command == "odd":
-        [result.append(x) for x in numbers if x % 2 != 0]
-    
-    return result
+        return list(filter(lambda x: x % 2 == 0, args[:-1]))
+    return list(filter(lambda x: x % 2 != 0, args[:-1]))
