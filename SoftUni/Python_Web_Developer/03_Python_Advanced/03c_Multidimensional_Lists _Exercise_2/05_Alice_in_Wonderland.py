e812c33f556fb8a0d8da28b50082e600c0b7f888
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 11:59:36 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/03c_Multidimensional_Lists _Exercise_2/05_Alice_in_Wonderland.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/03c_Multidimensional_Lists _Exercise_2/05_Alice_in_Wonderland.py
@@ -1,47 +1,40 @@
-size = int(input())
-wonderland = []
-alice_row, alice_col = 0, 0
+n = int(input())
 
-for row in range(size):
-    current_row = [x for x in input().split()]
-    wonderland.append(current_row)
-    if "A" in current_row:
-        alice_row, alice_col = row, current_row.index("A")
+matrix = []
+alice = []
 
-alice_left_wonderland = False
-alice_got_to_the_party = False
+for row in range(n):
+    matrix.append(input().split())
+    for col in range(n):
+        if matrix[row][col] == 'A':
+            matrix[row][col] = '*'
+            alice = [row, col]
+
+possible_moves = {'up' : (-1, 0), 
+                  'down' : (1, 0),
+                  'left' : (0, -1),
+                  'right' : (0, 1)}
 tea_bags = 0
 
-while True:
-    wonderland[alice_row][alice_col] = "*"
-    if alice_left_wonderland or alice_got_to_the_party:
+while tea_bags < 10:
+    command = input()
+    move = possible_moves[command]
+    row = alice[0] + move[0]
+    col = alice[1] + move[1]
+
+    if row < 0 or row >= n or col <0 or col >= n:
         break
-    direction = input()
-    if direction == "up":
-        next_row, next_col = alice_row - 1, alice_col
-    elif direction == "down":
-        next_row, next_col = alice_row + 1, alice_col
-    elif direction == "left":
-        next_row, next_col = alice_row, alice_col - 1
-    else:
-        next_row, next_col = alice_row, alice_col + 1
+    if matrix[row][col] == 'R':
+        matrix[row][col] = '*'
+        break
+    if matrix[row][col] not in '*.':
+        tea_bags += int(matrix[row][col])
+    matrix[row][col] = '*'
+    alice = [row, col]
 
-    if next_row in range(size) and next_col in range(size):
-        symbol = wonderland[next_row][next_col]
-        if symbol == "R":
-            wonderland[next_row][next_col] = "*"
-            alice_left_wonderland = True
-            break
-        elif symbol.isalnum():
-            tea_bags += int(symbol)
-        alice_row, alice_col = next_row, next_col
-        if tea_bags >= 10:
-            alice_got_to_the_party = True
-    else:
-        alice_left_wonderland = True
 
-if alice_got_to_the_party:
+if tea_bags >= 10:
     print("She did it! She went to the party.")
-if alice_left_wonderland:
+else:
     print("Alice didn't make it to the tea party.")
-[print(" ".join(element)) for element in wonderland]
+[print(' '.join(row)) for row in matrix]
