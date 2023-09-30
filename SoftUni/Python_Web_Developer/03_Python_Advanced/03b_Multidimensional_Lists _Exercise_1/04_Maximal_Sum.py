8f79c7b5d5d3e2b225325bd5f4ca5944559acb43
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 10:08:12 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/03b_Multidimensional_Lists _Exercise_1/04_Maximal_Sum.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/03b_Multidimensional_Lists _Exercise_1/04_Maximal_Sum.py
@@ -1,25 +1,22 @@
-import sys
+rows, cols = [int(x) for x in input().split()]
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
-rows, columns = [int(x) for x in input().split()]
-matrix = []
-max_sum = -sys.maxsize
-max_submatrix = []
-
-for _ in range(rows):
-    matrix.append([int(x) for x in input().split()])
+max_sum = float('-inf')
+max_row = 0
+max_col = 0
 
 for row in range(rows - 2):
-    for col in range(columns - 2):
-        submatrix = [
-            [matrix[row][col], matrix[row][col + 1], matrix[row][col + 2]],
-            [matrix[row + 1][col], matrix[row + 1][col + 1], matrix[row + 1][col + 2]],
-            [matrix[row + 2][col], matrix[row + 2][col + 1], matrix[row + 2][col + 2]]
-        ]
-        sum_elements = sum(submatrix[0]) + sum(submatrix[1]) + sum(submatrix[2])
-        if sum_elements > max_sum:
-            max_sum = sum_elements
-            max_submatrix = submatrix
+    for col in range(cols - 2):
+        current_sum = 0
+        for r in range(row, row + 3):
+            for c in range (col, col + 3):
+                current_sum += matrix[r][c]
+        if current_sum > max_sum:
+            max_sum = current_sum
+            max_row = row
+            max_col = col
 
 print(f"Sum = {max_sum}")
-for row in max_submatrix:
-    print(" ".join(str(x) for x in row))
+
+max_sub_matrix = [matrix[r][max_col:max_col + 3] for r in range(max_row, max_row + 3)]
+[print(*row) for row in max_sub_matrix]
