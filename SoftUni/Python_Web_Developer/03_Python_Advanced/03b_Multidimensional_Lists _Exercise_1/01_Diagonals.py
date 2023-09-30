ccf75c390239d9164af3d50bde67a03f7b88a29b
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 09:49:36 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/03b_Multidimensional_Lists _Exercise_1/01_Diagonals.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/03b_Multidimensional_Lists _Exercise_1/01_Diagonals.py
@@ -1,14 +1,8 @@
-matrix_size = int(input())
-matrix = []
-primary_diagonal = []
-secondary_diagonal = []
+n = int(input())
+matrix = [[int(x) for x in input().split(", ")] for _ in range(n)]
 
-for _ in range(matrix_size):
-    matrix.append([int(x) for x in input().split(", ")])
-
-for idx in range(matrix_size):
-    primary_diagonal.append(matrix[idx][idx])
-    secondary_diagonal.append(matrix[idx][matrix_size - 1 - idx])
+primary_diagonal = [matrix[i][i] for i in range(n)]
+secondary_diagonal = [matrix[i][- i - 1] for i in range(n)]
 
 print(f"Primary diagonal: {', '.join([str(x) for x in primary_diagonal])}. Sum: {sum(primary_diagonal)}")
 print(f"Secondary diagonal: {', '.join([str(x) for x in secondary_diagonal])}. Sum: {sum(secondary_diagonal)}")
