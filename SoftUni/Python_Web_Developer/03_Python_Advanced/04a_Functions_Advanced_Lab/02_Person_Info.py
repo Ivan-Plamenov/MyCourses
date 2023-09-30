3ac8bb6e50714ade26fb4d8aa6a00868049d1566
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-09-30 16:28:02 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/03_Python_Advanced/04a_Functions_Advanced_Lab/02_Person_Info.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/04a_Functions_Advanced_Lab/02_Person_Info.py
@@ -1,2 +1,2 @@
-def get_info(**kwargs):
-    return f"This is {kwargs['name']} from {kwargs['town']} and he is {kwargs['age']} years old"
+def get_info(name, age, town):
+    return f"This is {name} from {town} and he is {age} years old"
