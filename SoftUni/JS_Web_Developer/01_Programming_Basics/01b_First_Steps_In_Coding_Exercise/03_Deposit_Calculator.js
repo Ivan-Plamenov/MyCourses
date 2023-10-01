9e86e7631b0a2321a4897c0e90528c55b57fce76
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-01 14:05:22 +0300

    Formatting Improved

--- SoftUni/JS_Web_Developer/01_Programming_Basics/01b_First_Steps_In_Coding_Exercise/03_Deposit_Calculator.js
+++ SoftUni/JS_Web_Developer/01_Programming_Basics/01b_First_Steps_In_Coding_Exercise/03_Deposit_Calculator.js
@@ -1,8 +1,8 @@
-function calculatorDeposits(input){
-    let sumDeposit = Number(input[0]);
-    let lenghtDeposit = Number(input[1]);
-    let yearlyRate = Number(input[2]) / 100;
+function calculatorDeposits(input) {
+  let sumDeposit = Number(input[0]);
+  let lenghtDeposit = Number(input[1]);
+  let yearlyRate = Number(input[2]) / 100;
 
-    totalAmount = sumDeposit + lenghtDeposit * (( sumDeposit * yearlyRate)/12)
-    console.log(totalAmount)
+  totalAmount = sumDeposit + lenghtDeposit * ((sumDeposit * yearlyRate) / 12);
+  console.log(totalAmount);
 }
