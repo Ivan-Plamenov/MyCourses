212998839fd0e5691bdd6902047320b1436afcaa
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-01 13:31:54 +0300

    Formatting Improved

--- SoftUni/Python_Web_Developer/03_Python_Advanced/03a_Multidimensional_Lists _Lab/07_Square_with_Maximum Sum.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/03a_Multidimensional_Lists _Lab/07_Square_with_Maximum Sum.py
@@ -8,11 +8,11 @@ for row in range(rows):
     elements = [int(el) for el in input().split(", ")]
     matrix.append(elements)
 
-max_sum = float('-inf')
+max_sum = float("-inf")
 max_sum_sub_matrix = []
 
-for row_index in range(rows-1):
-    for col_index in range(cols-1):
+for row_index in range(rows - 1):
+    for col_index in range(cols - 1):
         current_element = matrix[row_index][col_index]
         next_element = matrix[row_index][col_index + 1]
         below_element = matrix[row_index + 1][col_index]
@@ -21,7 +21,10 @@ for row_index in range(rows-1):
 
         if max_sum < sum_elements:
             max_sum = sum_elements
-            max_sum_sub_matrix = [[current_element,next_element],[below_element,diagonal_element]]
+            max_sum_sub_matrix = [
+                [current_element, next_element],
+                [below_element, diagonal_element],
+            ]
 
 print(*max_sum_sub_matrix[0])
 print(*max_sum_sub_matrix[1])
