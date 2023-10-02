ea110db977b3537d81dabab8b528bc082cb3a419
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-02 17:32:19 +0300

    Formatting Improved

--- SoftUni/Python_Web_Developer/01_Programming_Basics/01b_First_Steps_In_Coding_Exercise/03_Deposit_ Calculator.py
+++ SoftUni/Python_Web_Developer/01_Programming_Basics/01b_First_Steps_In_Coding_Exercise/03_Deposit_ Calculator.py
@@ -2,5 +2,5 @@ deposit = float(input())
 lenght_of_deposit = int(input())
 yearly_interest = float(input())
 
-sum = deposit + lenght_of_deposit * ((deposit * yearly_interest/100) / 12)
+sum = deposit + lenght_of_deposit * ((deposit * yearly_interest / 100) / 12)
 print(sum)
