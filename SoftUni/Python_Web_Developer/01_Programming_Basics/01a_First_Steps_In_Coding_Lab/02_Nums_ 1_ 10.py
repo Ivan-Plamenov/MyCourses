0ffcdf73a86d2b907e8e1bc3efbc4de8b5f9ca6d
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-02 17:32:01 +0300

    Formatting Updated

--- SoftUni/Python_Web_Developer/01_Programming_Basics/01a_First_Steps_In_Coding_Lab/02_Nums_ 1_ 10.py
+++ SoftUni/Python_Web_Developer/01_Programming_Basics/01a_First_Steps_In_Coding_Lab/02_Nums_ 1_ 10.py
@@ -1,2 +1,2 @@
-for num in range(1,11):
+for num in range(1, 11):
     print(num)
--- SoftUni/Python_Web_Developer/01_Programming_Basics/01a_First_Steps_In_Coding_Lab/07_Projects_Creation.py
+++ SoftUni/Python_Web_Developer/01_Programming_Basics/01a_First_Steps_In_Coding_Lab/07_Projects_Creation.py
@@ -3,4 +3,6 @@ number_of_pojects = int(input())
 hours_needed = number_of_pojects * 3
 
 
-print(f"The architect {architect_name} will need {hours_needed} hours to complete {number_of_pojects} project/s.")
+print(
+    f"The architect {architect_name} will need {hours_needed} hours to complete {number_of_pojects} project/s."
+)
