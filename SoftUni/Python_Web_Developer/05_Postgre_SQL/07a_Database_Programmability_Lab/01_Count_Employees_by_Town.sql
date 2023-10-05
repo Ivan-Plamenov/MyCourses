9725659e46b81100d1932ce9867f4af5e6acfe1a
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-05 10:36:48 +0300

    Solutions Added

---
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/07a_Database_Programmability_Lab/01_Count_Employees_by_Town.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION fn_count_employees_by_town(town_name VARCHAR) RETURNS INT AS $$
+DECLARE BEGIN RETURN (
+        SELECT COUNT(*)
+        FROM employees AS e
+            JOIN addresses AS a USING(address_id)
+            JOIN towns AS t USING(town_id)
+        WHERE t.NAME = town_name
+    );
+END $$ LANGUAGE plpgsql;
