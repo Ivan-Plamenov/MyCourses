94721c8dddba1b3699aed9d8c2049669e1cd76d7
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-11 15:39:04 +0300

    Solution Added

--- SoftUni/Python_Web_Developer/03_Python_Advanced/00a_Exam_Preparation/2022.12.14_Retake/03_Student_Credits.py
+++ SoftUni/Python_Web_Developer/03_Python_Advanced/00a_Exam_Preparation/2022.12.14_Retake/03_Student_Credits.py
@@ -30,33 +30,3 @@ def students_credits(*courses):
         output.append(f"{course_name} - {earned_credits:.1f}")
 
     return "\n".join(output)
-
-
-print(
-    students_credits(
-        "Computer Science-12-300-250",
-        "Basic Algebra-15-400-200",
-        "Algorithms-25-500-490",
-    )
-)
-print(
-    students_credits(
-        "Discrete Maths-40-500-450",
-        "AI Development-20-400-400",
-        "Algorithms Advanced-50-700-630",
-        "Python Development-15-200-200",
-        "JavaScript Development-12-500-480",
-        "C++ Development-30-500-405",
-        "Game Engine Development-70-100-70",
-        "Mobile Development-25-250-225",
-        "QA-20-300-300",
-    )
-)
-print(
-    students_credits(
-        "Python Development-15-200-200",
-        "JavaScript Development-12-500-480",
-        "C++ Development-30-500-405",
-        "Java Development-10-300-150",
-    )
-)
