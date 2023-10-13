569fd0c5a7d632b0a0946e0b042b7e3aab37d5b7
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-13 11:45:30 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01c_Exam_Preparation_3/2_4_Delete.sql
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01c_Exam_Preparation_3/2_4_Delete.sql
@@ -1,4 +1,4 @@
--- Delete board games associated with publishers linked to addresses from towns starting with 'L'
+-- Delete records from referencing tables first
 DELETE FROM board_games
 WHERE publisher_id IN (
         SELECT id
@@ -9,13 +9,12 @@ WHERE publisher_id IN (
                 WHERE town LIKE 'L%'
             )
     );
--- Delete publishers associated with addresses from towns starting with 'L'
 DELETE FROM publishers
 WHERE address_id IN (
         SELECT id
         FROM addresses
         WHERE town LIKE 'L%'
     );
--- Finally, delete addresses from towns starting with 'L'
+-- Delete records from addresses table
 DELETE FROM addresses
 WHERE town LIKE 'L%';
