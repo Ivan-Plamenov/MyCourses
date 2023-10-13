dfcbf04402cafe7f2ffe47b7e8041b129d944687
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-13 11:43:51 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01c_Exam_Preparation_3/2_3_Update.sql
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01c_Exam_Preparation_3/2_3_Update.sql
@@ -1,9 +1,9 @@
--- Update max players count for board games with a player range of [2, 2]
+-- Increase the max_players by 1 for rows where min_players is 2 and max_players is 2
 UPDATE players_ranges
 SET max_players = max_players + 1
 WHERE min_players = 2
     AND max_players = 2;
--- Append ' V2' to the names of board games published in 2020 or later
+-- Append ' V2' to the game name for board games released in the year 2020 or later
 UPDATE board_games
-SET name = name || ' V2'
+SET "name" = "name" || ' V2'
 WHERE release_year >= 2020;
