8caada47e22b1730bef83698a289f550fcd5a893
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-13 11:25:33 +0300

    Solution Added

--- SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/3_7_Number_of_Courses_for_Each_Car.sql
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/3_7_Number_of_Courses_for_Each_Car.sql
@@ -1,22 +1,14 @@
--- Retrieve information about cars, count of courses, and average bill
-WITH CourseInfo AS (
-    SELECT c.id AS car_id,
-        c.make,
-        c.mileage,
-        COUNT(co.id) AS count_of_courses,
-        ROUND(AVG(co.bill)::numeric, 2) AS average_bill
-    FROM cars c
-        LEFT JOIN courses co ON c.id = co.car_id
-    GROUP BY c.id,
-        c.make,
-        c.mileage
-    HAVING COUNT(co.id) <> 2
-)
-SELECT car_id,
-    make,
-    mileage,
-    count_of_courses,
-    average_bill
-FROM CourseInfo
+-- Retrieves car ID, make, mileage, number of courses (trips), and the average bill for each car. Only considers cars with a number of courses not equal to 2. Results are ordered by the number of courses in descending order and then by car ID.
+SELECT c."id" AS car_id,
+    c.make,
+    c.mileage,
+    COUNT(cou."id") AS count_of_courses,
+    ROUND(AVG(cou.bill)::NUMERIC, 2) AS average_bill
+FROM cars AS c
+    LEFT JOIN courses AS cou ON cou.car_id = c."id"
+GROUP BY c."id",
+    c.make,
+    c.mileage
+HAVING COUNT(cou."id") != 2
 ORDER BY count_of_courses DESC,
-    car_id ASC;
+    car_id;
