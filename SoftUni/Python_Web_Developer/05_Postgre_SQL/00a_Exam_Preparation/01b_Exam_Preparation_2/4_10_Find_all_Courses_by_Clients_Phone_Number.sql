16e3dacce2025c224585d401f618cb1c7de997b2
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-13 11:32:01 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/4_10_Find_all_Courses_by_Clients_Phone_Number.sql
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/4_10_Find_all_Courses_by_Clients_Phone_Number.sql
@@ -1,9 +1,15 @@
--- Create a user-defined function to count courses for a client based on phone number
-CREATE OR REPLACE FUNCTION fn_courses_by_client(phone_num VARCHAR(20)) RETURNS INTEGER AS $$ BEGIN RETURN (
-        SELECT COUNT(*)
-        FROM courses c
-            JOIN clients cl ON c.client_id = cl.id
-        WHERE cl.phone_number = phone_num
-    );
+-- Defines a function that returns the number of courses booked by a client based on their phone number.
+CREATE OR REPLACE FUNCTION fn_courses_by_client(phone_num VARCHAR(20)) RETURNS INT AS $$
+DECLARE course_count INT;
+BEGIN
+SELECT COUNT(*) INTO course_count
+FROM courses AS cou
+    JOIN clients AS cl ON cou.client_id = cl."id"
+WHERE cl.phone_number = phone_num;
+RETURN course_count;
 END;
 $$ LANGUAGE plpgsql;
+-- Calls the function with three different phone numbers and fetches the number of courses for each client.
+SELECT fn_courses_by_client('(803) 6386812') as "number_of_courses";
+SELECT fn_courses_by_client('(831) 1391236') as "number_of_courses";
+SELECT fn_courses_by_client('(704) 2502909') as "number_of_courses";
