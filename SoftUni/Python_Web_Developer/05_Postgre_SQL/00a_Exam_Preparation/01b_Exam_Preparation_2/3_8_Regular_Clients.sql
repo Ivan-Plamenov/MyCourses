cb8a1b489ac773f435f713c69e883bfb0c81c4fe
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-13 11:27:04 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/3_8_Regular_Clients.sql
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/3_8_Regular_Clients.sql
@@ -1,16 +1,10 @@
--- Extract details of regular clients with the second letter 'a' in their full name
-WITH RegularClients AS (
-    SELECT c.full_name,
-        COUNT(DISTINCT co.car_id) AS count_of_cars,
-        SUM(co.bill) AS total_sum
-    FROM clients c
-        JOIN courses co ON c.id = co.client_id
-    WHERE SUBSTRING(c.full_name, 2, 1) = 'a'
-    GROUP BY c.full_name
-    HAVING COUNT(DISTINCT co.car_id) > 1
-)
-SELECT full_name,
-    count_of_cars,
-    total_sum
-FROM RegularClients
-ORDER BY full_name;
+-- Retrieves each client's full name, number of cars, and total sum of their bills. Only includes clients with the second letter of their full name as 'a' and those who have more than one car. Results are ordered by the client's full name.
+SELECT cl.full_name,
+    COUNT(cl."id") AS count_of_cars,
+    SUM(COALESCE (cou.bill, 0)) AS total_sum
+FROM clients AS cl
+    JOIN courses AS cou ON cou.client_id = cl."id"
+WHERE SUBSTRING(cl.full_name, 2, 1) = 'a'
+GROUP BY cl."id"
+HAVING COUNT(cl."id") > 1
+ORDER BY cl.full_name;
