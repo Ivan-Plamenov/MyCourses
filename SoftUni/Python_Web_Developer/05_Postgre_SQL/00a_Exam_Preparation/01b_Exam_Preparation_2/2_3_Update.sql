8c76bd9c6b46b54955e1515cb1c2d3b744f45f5f
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-13 11:20:31 +0300

    Solution Uptaded

--- SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/2_3_Update.sql
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/2_3_Update.sql
@@ -1,9 +1,12 @@
--- Update the "condition" of cars meeting the specified criteria
+-- Updates the condition of cars to 'C' if:
+-- 1. They have mileage >= 800,000 or mileage is not specified.
+-- 2. They were manufactured on or before 2010.
+-- 3. The car make isn't 'Mercedes-Benz'.
 UPDATE cars
-SET condition = 'C'
+SET "condition" = 'C'
 WHERE (
         mileage >= 800000
         OR mileage IS NULL
     )
-    AND year <= 2010
-    AND make <> 'Mercedes-Benz';
+    AND "year" <= 2010
+    AND make != 'Mercedes-Benz';
