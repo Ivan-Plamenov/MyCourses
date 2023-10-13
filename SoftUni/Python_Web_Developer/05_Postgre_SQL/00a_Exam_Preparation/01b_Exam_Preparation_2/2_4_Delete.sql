45ce0eefcd0f5692a86cd7199aaa3344c8a4be93
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-13 11:21:10 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/2_4_Delete.sql
+++ SoftUni/Python_Web_Developer/05_Postgre_SQL/00a_Exam_Preparation/01b_Exam_Preparation_2/2_4_Delete.sql
@@ -1,7 +1,10 @@
--- Delete clients who meet the specified criteria
-DELETE FROM clients
-WHERE LENGTH(full_name) > 3
-    AND id NOT IN (
-        SELECT DISTINCT client_id
-        FROM courses
-    );
+-- Deletes clients who:
+-- 1. Do not have any associated courses.
+-- 2. Have a full name longer than 3 characters.
+DELETE FROM clients AS cl
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM courses AS cou
+        WHERE cou.client_id = cl."id"
+    )
+    AND LENGTH(cl.full_name) > 3;
