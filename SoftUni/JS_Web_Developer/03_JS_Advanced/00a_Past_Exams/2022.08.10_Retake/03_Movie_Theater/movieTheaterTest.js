975e494812d9a8bba75f2fdaa5f0802f449f22bf
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-15 15:32:42 +0300

    Solution Added

---
+++ SoftUni/JS_Web_Developer/03_JS_Advanced/00a_Past_Exams/2022.08.10_Retake/03_Movie_Theater/movieTheaterTest.js
@@ -0,0 +1,72 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const movieTheater = require("./movieTheater");
+
+describe("movieTheater", function () {
+  describe("ageRestrictions()", function () {
+    it("should handle movie rating G", function () {
+      expect(movieTheater.ageRestrictions("G")).to.equal(
+        "All ages admitted to watch the movie"
+      );
+    });
+
+    it("should handle movie rating PG", function () {
+      expect(movieTheater.ageRestrictions("PG")).to.equal(
+        "Parental guidance suggested! Some material may not be suitable for pre-teenagers"
+      );
+    });
+
+    // ... [similar tests for R and NC-17]
+
+    it("should handle unknown movie ratings", function () {
+      expect(movieTheater.ageRestrictions("Unknown")).to.equal(
+        "There are no age restrictions for this movie"
+      );
+    });
+  });
+
+  describe("moneySpent()", function () {
+    it("should calculate cost correctly without discount", function () {
+      expect(movieTheater.moneySpent(1, ["Popcorn"], ["Soda"])).to.equal(
+        "The total cost for the purchase is 22.00"
+      );
+    });
+
+    it("should calculate cost correctly with discount", function () {
+      expect(
+        movieTheater.moneySpent(4, ["Nachos", "Popcorn"], ["Soda", "Water"])
+      ).to.equal(
+        "The total cost for the purchase with applied discount is 54.40"
+      );
+    });
+
+    it("should throw error for invalid ticket input", function () {
+      expect(() => movieTheater.moneySpent("1", [], [])).to.throw(
+        "Invalid input"
+      );
+    });
+  });
+
+  describe("reservation()", function () {
+    it("should return the row with the most seats when multiple rows can accommodate", function () {
+      const rows = [
+        { rowNumber: 1, freeSeats: 7 },
+        { rowNumber: 2, freeSeats: 8 },
+      ];
+      expect(movieTheater.reservation(rows, 7)).to.equal(2);
+    });
+
+    it("should throw error for invalid rowArray input", function () {
+      expect(() => movieTheater.reservation("invalid", 1)).to.throw(
+        "Invalid input"
+      );
+    });
+
+    it("should throw error for invalid neededSeatsCount input", function () {
+      expect(() => movieTheater.reservation([], "invalid")).to.throw(
+        "Invalid input"
+      );
+    });
+  });
+});
