074d48c5694d467d7366b6e8f51c0903cfd0cb50
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-15 17:25:46 +0300

    Solution Updated

--- SoftUni/JS_Web_Developer/03_JS_Advanced/09b_Prototypes_and_Inheritance_Exercise/07_Computer.js
+++ SoftUni/JS_Web_Developer/03_JS_Advanced/09b_Prototypes_and_Inheritance_Exercise/07_Computer.js
@@ -1,99 +1,114 @@
 function createComputerHierarchy() {
-    class Computer {
-        constructor(manuf, speed, ram, disk) {
-            if (new.target == Computer) {
-                throw new Error(
-                    "Cannot make an instance of abstract class Computer"
-                );
-            }
-            this.manufacturer = manuf;
-            this.processorSpeed = speed;
-            this.ram = ram;
-            this.hardDiskSpace = disk;
-        }
+  class Keyboard {
+    constructor(manufacturer, responseTime) {
+      this.manufacturer = manufacturer;
+      this.responseTime = responseTime;
+    }
+  }
+
+  class Monitor {
+    constructor(manufacturer, width, height) {
+      this.manufacturer = manufacturer;
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  class Battery {
+    constructor(manufacturer, expectedLife) {
+      this.manufacturer = manufacturer;
+      this.expectedLife = expectedLife;
+    }
+  }
+
+  class Computer {
+    constructor(manufacturer, processorSpeed, ram, hardDiskSpace) {
+      if (new.target === Computer) {
+        throw new Error("Cannot instantiate an abstract class.");
+      }
+      this.manufacturer = manufacturer;
+      this.processorSpeed = processorSpeed;
+      this.ram = ram;
+      this.hardDiskSpace = hardDiskSpace;
+    }
+  }
+
+  class Laptop extends Computer {
+    constructor(
+      manufacturer,
+      processorSpeed,
+      ram,
+      hardDiskSpace,
+      weight,
+      color,
+      battery
+    ) {
+      super(manufacturer, processorSpeed, ram, hardDiskSpace);
+      this.weight = weight;
+      this.color = color;
+      this._battery = null;
+
+      this.battery = battery; // Using the setter for validation
     }
 
-    class Laptop extends Computer {
-        constructor(manuf, speed, ram, disk, weight, color, battery) {
-            super(manuf, speed, ram, disk);
-            this.weight = weight;
-            this.color = color;
-            this._battery = battery;
-        }
-
-        get battery() {
-            return this._battery;
-        }
-
-        set battery(obj) {
-            if (!(obj instanceof Battery)) {
-                throw new TypeError("Use the appropriate component");
-            } else {
-                this._battery = obj;
-            }
-        }
+    get battery() {
+      return this._battery;
     }
 
-    class Desktop extends Computer {
-        constructor(manuf, speed, ram, disk, keyboard, monitor) {
-            super(manuf, speed, ram, disk);
-            this._keyboard = keyboard;
-            this._monitor = monitor;
-        }
+    set battery(newBattery) {
+      if (!(newBattery instanceof Battery)) {
+        throw new TypeError("Invalid battery type");
+      }
+      this._battery = newBattery;
+    }
+  }
 
-        get keyboard() {
-            return this._keyboard;
-        }
+  class Desktop extends Computer {
+    constructor(
+      manufacturer,
+      processorSpeed,
+      ram,
+      hardDiskSpace,
+      keyboard,
+      monitor
+    ) {
+      super(manufacturer, processorSpeed, ram, hardDiskSpace);
+      this.keyboard = null;
+      this.monitor = null;
 
-        get monitor() {
-            return this._monitor;
-        }
-
-        set keyboard(obj) {
-            if (!(obj instanceof Keyboard)) {
-                throw new TypeError("Use the appropriate component");
-            } else {
-                this._keyboard = obj;
-            }
-        }
-
-        set monitor(obj) {
-            if (!(obj instanceof Monitor)) {
-                throw new TypeError("Use the appropriate component");
-            } else {
-                this._monitor = obj;
-            }
-        }
+      this.keyboard = keyboard; // Using the setter for validation
+      this.monitor = monitor; // Using the setter for validation
     }
 
-    class Keyboard {
-        constructor(manuf, time) {
-            this.manufacturer = manuf;
-            this.responseTime = time;
-        }
+    get keyboard() {
+      return this._keyboard;
     }
 
-    class Monitor {
-        constructor(manuf, width, height) {
-            this.manufacturer = manuf;
-            this.width = width;
-            this.height = height;
-        }
+    set keyboard(newKeyboard) {
+      if (!(newKeyboard instanceof Keyboard)) {
+        throw new TypeError("Invalid keyboard type");
+      }
+      this._keyboard = newKeyboard;
     }
 
-    class Battery {
-        constructor(manuf, expectedLife) {
-            this.manufacturer = manuf;
-            this.expectedLife = expectedLife;
-        }
+    get monitor() {
+      return this._monitor;
     }
 
-    return {
-        Computer,
-        Laptop,
-        Desktop,
-        Monitor,
-        Keyboard,
-        Battery,
-    };
+    set monitor(newMonitor) {
+      if (!(newMonitor instanceof Monitor)) {
+        throw new TypeError("Invalid monitor type");
+      }
+      this._monitor = newMonitor;
+    }
+  }
+
+  return {
+    Battery,
+    Keyboard,
+    Monitor,
+    Computer,
+    Laptop,
+    Desktop,
+  };
 }
