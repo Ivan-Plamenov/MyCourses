a2510944766c3d4a0c8761a48ec0ba40df5056dc
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-15 16:37:56 +0300

    Solution Updated

--- SoftUni/JS_Web_Developer/03_JS_Advanced/01b_Syntax_Functions_and_Statements_Exercise/08_Validity_Checker.js
+++ SoftUni/JS_Web_Developer/03_JS_Advanced/01b_Syntax_Functions_and_Statements_Exercise/08_Validity_Checker.js
@@ -1,22 +1,22 @@
-function validityChecker(x1, x2, y1, y2) {
-    function distance(_x1, _y1, _x2, _y2) {
-        let dist = Math.sqrt(Math.pow(_x2 - _x1, 2) + Math.pow(_y2 - _y1, 2));
-        return Number.isInteger(dist);
-    }
+function distanceValidityChecker(x1, y1, x2, y2) {
+  function calculateDistance(x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  }
 
-    if (distance(x1, y1, 0, 0)) {
-        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+  function checkValidDistance(x1, y1, x2, y2) {
+    const distance = calculateDistance(x1, y1, x2, y2);
+    return Number.isInteger(distance);
+  }
+
+  function printValidity(x1, y1, x2, y2) {
+    if (checkValidDistance(x1, y1, x2, y2)) {
+      console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
     } else {
-        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+      console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
     }
-    if (distance(x2, y2, 0, 0)) {
-        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
-    } else {
-        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
-    }
-    if (distance(x1, y1, x2, y2)) {
-        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
-    } else {
-        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
-    }
+  }
+
+  printValidity(x1, y1, 0, 0);
+  printValidity(x2, y2, 0, 0);
+  printValidity(x1, y1, x2, y2);
 }
