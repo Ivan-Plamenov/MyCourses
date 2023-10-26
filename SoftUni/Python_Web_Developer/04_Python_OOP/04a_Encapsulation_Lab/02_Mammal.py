01c46f1fef9f60ac69d9e71cedcd545ef127f572
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-26 11:59:38 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/04_Python_OOP/04a_Encapsulation_Lab/02_Mammal.py
+++ SoftUni/Python_Web_Developer/04_Python_OOP/04a_Encapsulation_Lab/02_Mammal.py
@@ -1,17 +1,16 @@
-# 75 / 100
 class Mammal:
-    kingdom = "animals"
+    __kingdom = "animals"
 
-    def __init__(self, name, type, sound):
-        self.name = name
-        self.type = type
-        self.sound = sound
+    def __init__(self, _name: str, _type: str, _sound: str) -> None:
+        self.name = _name
+        self.type = _type
+        self.sound = _sound
 
     def make_sound(self):
         return f"{self.name} makes {self.sound}"
 
     def get_kingdom(self):
-        return Mammal.kingdom
+        return Mammal.__kingdom
 
     def info(self):
         return f"{self.name} is of type {self.type}"
