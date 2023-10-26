63c368223040b554082d56fffca621b5c05914d8
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-26 12:39:23 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/04_Python_OOP/06b_Polymorphism_and_Abstraction_Exercise/03_Account.py
+++ SoftUni/Python_Web_Developer/04_Python_OOP/06b_Polymorphism_and_Abstraction_Exercise/03_Account.py
@@ -1,31 +1,36 @@
-# 81 / 100
 class Account:
-    def __init__(self, owner, starting_amount=0):
-        self.owner = owner
-        self.starting_amount = starting_amount
+    def __init__(self, _owner: str, _amount=0) -> None:
+        self.owner = _owner
+        self.amount = _amount
         self._transactions = []
 
-    def handle_transaction(self, transaction_amount):
-        new_balance = self.balance + transaction_amount
-        if new_balance < 0:
-            raise ValueError("Sorry cannot go in debt!")
-        self._transactions.append(transaction_amount)
-        return f"New balance: {new_balance}"
-
-    def add_transaction(self, amount):
-        if not isinstance(amount, int):
-            raise ValueError("Please use int for amount")
-        return self.handle_transaction(amount)
+    def add_transaction(self, _amount: int):
+        if isinstance(_amount, int):
+            self._transactions.append(_amount)
+        else:
+            raise ValueError("please use int for amount")
 
     @property
     def balance(self):
-        return self.starting_amount + sum(self._transactions)
+        result = 0
+        result += self.amount
+        for transaction in self._transactions:
+            result += transaction
+        return result
 
-    def __str__(self):
-        return f"Account of {self.owner} with starting amount: {self.starting_amount}"
+    def validate_transaction(self, _account, _amount_to_add: int):
+        try:
+            _account._transactions.append(_amount_to_add)
+            if _account.balance < 0:
+                raise ValueError("sorry cannot go in debt!")
+        except ValueError:
+            _account._transactions.pop()
 
-    def __repr__(self):
-        return f"Account({self.owner}, {self.starting_amount})"
+    def __str__(self) -> str:
+        return f"Account of {self.owner} with starting amount: {self.amount}"
+
+    def __repr__(self) -> str:
+        return f"Account({self.owner}, {self.amount})"
 
     def __len__(self):
         return len(self._transactions)
@@ -33,35 +38,51 @@ class Account:
     def __getitem__(self, index):
         return self._transactions[index]
 
-    def __iter__(self):
-        return iter(self._transactions)
-
     def __reversed__(self):
-        return reversed(self._transactions)
-
-    def __gt__(self, other):
-        return self.balance > other.balance
-
-    def __ge__(self, other):
-        return self.balance >= other.balance
-
-    def __lt__(self, other):
-        return self.balance < other.balance
-
-    def __le__(self, other):
-        return self.balance <= other.balance
-
-    def __eq__(self, other):
-        return self.balance == other.balance
-
-    def __ne__(self, other):
-        return self.balance != other.balance
+        return self._transactions[::-1]
 
     def __add__(self, other):
-        new_owner = f"{self.owner}&{other.owner}"
-        new_starting_amount = self.starting_amount + other.starting_amount
-        new_transactions = self._transactions + other._transactions
-        return Account(new_owner, new_starting_amount)
+        owner = f"{self.owner}&{other.owner}"
+        amount = self.amount + other.amount
+        new_account = Account(owner, amount)
+        new_account._transactions = self._transactions + other._transactions
+        return new_account
+
+    def __lt__(self, other):
+        if self.balance < other.balance:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.balance > other.balance:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if self.balance <= other.balance:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        if self.balance >= other.balance:
+            return True
+        else:
+            return False
+
+    def __eq__(self, other):
+        if self.balance == other.balance:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        if self.balance != other.balance:
+            return True
+        else:
+            return False
 
 
 # Example usage:
