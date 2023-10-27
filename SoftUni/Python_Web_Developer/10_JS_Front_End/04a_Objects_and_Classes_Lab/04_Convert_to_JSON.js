52eed876757c11972e88aaa7a3aac8251447a6db
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-27 14:55:22 +0300

    Solution Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/04a_Objects_and_Classes_Lab/04_Convert_to_JSON.js
@@ -0,0 +1,14 @@
+function convertToJSON(firstName, lastName, hairColor) {
+  const person = {
+    name: firstName,
+    lastName: lastName,
+    hairColor: hairColor,
+  };
+
+  const jsonString = JSON.stringify(person);
+  console.log(jsonString);
+}
+
+// Test Cases
+convertToJSON("George", "Jones", "Brown");
+convertToJSON("Peter", "Smith", "Blond");
