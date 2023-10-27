a942effd1d553c2bdb18125f1d5559fb1713eb12
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-27 13:44:34 +0300

    Solution Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/02b_JS_Arrays_and_Strings_Exercise/04_Sorting_Numbers.js
@@ -0,0 +1,31 @@
+function sortNumbers(numbers) {
+  // Sort the numbers in ascending order
+  const sortedNumbers = numbers.sort((a, b) => a - b);
+
+  const result = [];
+
+  // Use two pointers to extract the smallest and largest numbers
+  let left = 0;
+  let right = sortedNumbers.length - 1;
+
+  while (left <= right) {
+    // Add the smallest number to the result
+    result.push(sortedNumbers[left]);
+
+    // Add the largest number to the result
+    if (left !== right) {
+      result.push(sortedNumbers[right]);
+    }
+
+    // Move the pointers
+    left++;
+    right--;
+  }
+
+  return result;
+}
+
+// Test Cases
+const numbersArray = [1, 65, 3, 52, 48, 63, 31, -3, 18, 56];
+const sortedArray = sortNumbers(numbersArray);
+console.log(sortedArray);
