da8639595724162fc396423ca1d8a2c837ad1055
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-27 12:51:10 +0300

    Soluition Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/01a_JS_Syntax_Fundamentals_Lab/01_Multiply_Number_by_2.js
@@ -0,0 +1,9 @@
+function multiplyByTwo(num) {
+  var result = num * 2;
+  console.log(result);
+}
+
+// Test Cases
+multiplyByTwo(2); // Output: 4
+multiplyByTwo(5); // Output: 10
+multiplyByTwo(20); // Output: 40
