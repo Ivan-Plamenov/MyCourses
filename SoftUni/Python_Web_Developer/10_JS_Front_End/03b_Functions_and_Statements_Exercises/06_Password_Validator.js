2b0b13dea2b885d8458697b0a73b1af748bcdb88
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-27 14:28:05 +0300

    Solution Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/03b_Functions_and_Statements_Exercises/06_Password_Validator.js
@@ -0,0 +1,34 @@
+function validatePassword(password) {
+  let isValid = true;
+  let digitsCount = 0;
+
+  if (password.length < 6 || password.length > 10) {
+    console.log("Password must be between 6 and 10 characters");
+    isValid = false;
+  }
+
+  if (!/^[a-zA-Z0-9]+$/.test(password)) {
+    console.log("Password must consist only of letters and digits");
+    isValid = false;
+  }
+
+  for (let char of password) {
+    if (/\d/.test(char)) {
+      digitsCount++;
+    }
+  }
+
+  if (digitsCount < 2) {
+    console.log("Password must have at least 2 digits");
+    isValid = false;
+  }
+
+  if (isValid) {
+    console.log("Password is valid");
+  }
+}
+
+// Test Cases
+validatePassword("logIn");
+validatePassword("MyPass123");
+validatePassword("Pa$s$s");
