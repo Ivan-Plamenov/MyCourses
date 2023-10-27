8197db8b60062538017179f8a711d5a5e1f3998b
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-27 14:59:37 +0300

    Solution Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/04a_Objects_and_Classes_Lab/06_Meetings.js
@@ -0,0 +1,31 @@
+function manageMeetings(input) {
+  const meetings = {};
+  const successfulMeetings = [];
+
+  input.forEach((meeting) => {
+    const [day, name] = meeting.split(" ");
+
+    if (meetings.hasOwnProperty(day)) {
+      console.log(`Conflict on ${day}!`);
+    } else {
+      meetings[day] = name;
+      console.log(`Scheduled for ${day}`);
+    }
+  });
+
+  for (const day in meetings) {
+    successfulMeetings.push(`${day} -> ${meetings[day]}`);
+  }
+
+  console.log(successfulMeetings.join("\n"));
+}
+
+// Test Cases
+const meetingData = [
+  "Monday Peter",
+  "Wednesday Bill",
+  "Monday Tim",
+  "Friday Tim",
+];
+
+manageMeetings(meetingData);
