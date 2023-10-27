da284f623293ec6e938fda40481c083178951210
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-27 14:45:18 +0300

    Solution Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/03c_Functions_and_Statements_More_Exercises/05_Print_DNA.js
@@ -0,0 +1,30 @@
+function printDNAHelix(length) {
+  const sequence = "ATCGTTAGGG";
+  let sequenceIndex = 0;
+
+  for (let i = 0; i < length; i++) {
+    const currentRow = i % 4;
+
+    if (currentRow === 0) {
+      console.log(
+        `**${sequence[sequenceIndex++]}${sequence[sequenceIndex++]}**`
+      );
+    } else if (currentRow === 1 || currentRow === 3) {
+      console.log(
+        `*${sequence[sequenceIndex++]}--${sequence[sequenceIndex++]}*`
+      );
+    } else {
+      console.log(
+        `${sequence[sequenceIndex++]}----${sequence[sequenceIndex++]}`
+      );
+    }
+
+    if (sequenceIndex === sequence.length) {
+      sequenceIndex = 0;
+    }
+  }
+}
+
+// Test Cases
+printDNAHelix(4);
+printDNAHelix(10);
