ab98884c4287b42a6a8f1f3f593e73c00b4c6c42
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-27 14:25:09 +0300

    Solution Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/03b_Functions_and_Statements_Exercises/02_Add_and_Subtract.js
@@ -0,0 +1,10 @@
+function sumAndSubtract(num1, num2, num3) {
+  let sum = num1 + num2;
+  let result = sum - num3;
+  return result;
+}
+
+// Test Cases
+console.log(sumAndSubtract(23, 6, 10)); // Output: 19
+console.log(sumAndSubtract(1, 17, 30)); // Output: -12
+console.log(sumAndSubtract(42, 58, 100)); // Output: 0
