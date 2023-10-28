ea7e3660d5ec25c29f6b0043c1e0df63a56ce550
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-28 10:13:19 +0300

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/05a_DOM_Introduction_Lab/02_Collect_List_Items/CollectListItems.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/05a_DOM_Introduction_Lab/02_Collect_List_Items/CollectListItems.js
@@ -1,3 +1,6 @@
 function extractText() {
-    // TODO
+  let ulElement = document.getElementById("items");
+
+  let textareaElement = document.getElementById("result");
+  textareaElement.textContent = ulElement.textContent;
 }
