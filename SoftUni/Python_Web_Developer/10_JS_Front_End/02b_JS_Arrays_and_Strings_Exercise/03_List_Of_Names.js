3905196a1974a32b2ef86581db0108e368855469
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-28 09:39:09 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/10_JS_Front_End/02b_JS_Arrays_and_Strings_Exercise/03_List_Of_Names.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/02b_JS_Arrays_and_Strings_Exercise/03_List_Of_Names.js
@@ -1,13 +1,8 @@
-function printSortedNames(names) {
-  // Sort the names alphabetically in ascending order
-  const sortedNames = names.sort();
-
-  // Iterate through the sorted names and print them with numbers
-  for (let i = 0; i < sortedNames.length; i++) {
-    console.log(`${i + 1}.${sortedNames[i]}`);
+function printSortedNames(input) {
+  input.sort((a, b) => a.localeCompare(b));
+  for (let i = 0; i < input.length; i++) {
+    console.log(`${i + 1}.${input[i]}`);
   }
 }
 
 // Test Cases
-const namesArray = ["John", "Bob", "Christina", "Ema"];
-printSortedNames(namesArray);
