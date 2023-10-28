399b3582baec7ee886b28ace8439feaa97cbc9cd
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-28 10:05:54 +0300

    Solution Added

---
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/04c_Objects_and_Classes_More_Exercises/03_Class_Laptop.js
@@ -0,0 +1,29 @@
+class Laptop {
+  constructor(info, quality) {
+    this.info = {
+      producer: info.producer,
+      age: info.age,
+      brand: info.brand,
+    };
+    this.quality = quality;
+    this.isOn = false;
+  }
+
+  turnOn() {
+    this.isOn = true;
+    this.quality -= 1;
+  }
+
+  turnOff() {
+    this.isOn = false;
+    this.quality -= 1;
+  }
+
+  showInfo() {
+    return JSON.stringify(this.info);
+  }
+
+  get price() {
+    return 800 - this.info.age * 2 + this.quality * 0.5;
+  }
+}
