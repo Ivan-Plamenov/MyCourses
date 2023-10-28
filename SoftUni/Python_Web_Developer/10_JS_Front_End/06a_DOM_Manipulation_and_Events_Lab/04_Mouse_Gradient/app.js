ad3662cd707b033a24e48ed818ea17461cd3d138
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-28 10:46:19 +0300

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/06a_DOM_Manipulation_and_Events_Lab/04_Mouse_Gradient/app.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/06a_DOM_Manipulation_and_Events_Lab/04_Mouse_Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  const divEl = document.getElementById("result");
+  const gradientEl = document.getElementById("gradient");
+  gradientEl.addEventListener("mousemove", calc);
+  gradientEl.addEventListener("mouseleave", () => (divEl.textContent = ""));
+
+  function calc(event) {
+    let position = Math.floor(
+      (event.offsetX / (event.target.clientWidth - 1)) * 100
+    );
+    divEl.textContent = `${position}%`;
+  }
 }
