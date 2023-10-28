14aa3a5666951d7d6810695d93deb35fa62dd15e
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-28 09:38:10 +0300

    Solution Updated

--- SoftUni/Python_Web_Developer/10_JS_Front_End/02b_JS_Arrays_and_Strings_Exercise/07_String_Substring.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/02b_JS_Arrays_and_Strings_Exercise/07_String_Substring.js
@@ -1,21 +1,18 @@
-function findWordInText(word, text) {
-  // Convert both the word and text to lowercase for case-insensitive comparison
-  const lowercaseWord = word.toLowerCase();
-  const lowercaseText = text.toLowerCase();
+function stringSubstring(word, text) {
+  let textArr = text.split(" ");
+  flag = true;
 
-  // Check if the lowercase text contains the lowercase word
-  if (lowercaseText.includes(lowercaseWord)) {
-    return lowercaseWord;
-  } else {
-    return `${word} not found!`;
+  for (el of textArr) {
+    if (el.toLowerCase() == word) {
+      console.log(word);
+      flag = false;
+      break;
+    }
+  }
+
+  if (flag) {
+    console.log(`${word} not found!`);
   }
 }
 
 // Test Cases
-const word1 = "javascript";
-const text1 = "JavaScript is the best programming language";
-console.log(findWordInText(word1, text1));
-
-const word2 = "python";
-const text2 = "JavaScript is the best programming language";
-console.log(findWordInText(word2, text2));
