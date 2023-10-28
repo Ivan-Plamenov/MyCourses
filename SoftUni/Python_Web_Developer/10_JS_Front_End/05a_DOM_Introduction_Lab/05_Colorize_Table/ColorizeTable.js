ee36f0da7642557b1613232a929e8ea62cb12c26
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-28 10:24:31 +0300

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/05a_DOM_Introduction_Lab/05_Colorize_Table/ColorizeTable.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/05a_DOM_Introduction_Lab/05_Colorize_Table/ColorizeTable.js
@@ -1,3 +1,4 @@
 function colorize() {
-    // TODO
+  let tableRow = Array.from(document.querySelectorAll("tr:nth-of-type(2n)"));
+  tableRow.forEach((td) => (td.style.backgroundColor = "teal"));
 }
