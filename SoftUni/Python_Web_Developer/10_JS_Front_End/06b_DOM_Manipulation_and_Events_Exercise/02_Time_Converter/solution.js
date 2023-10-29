9e1ebfc89e1cd545f90a6de0c70536211a394a5b
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-29 19:05:08 +0200

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/06b_DOM_Manipulation_and_Events_Exercise/02_Time_Converter/solution.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/06b_DOM_Manipulation_and_Events_Exercise/02_Time_Converter/solution.js
@@ -1,4 +1,58 @@
 function attachEventsListeners() {
+  var days = document.getElementById("days");
+  var hours = document.getElementById("hours");
+  var minutes = document.getElementById("minutes");
+  var seconds = document.getElementById("seconds");
+  let value = 0;
+  let type = "";
 
-    console.log('TODO:...');
+  var buttons = Array.from(document.querySelectorAll("input[type=button]"));
+  buttons.forEach((button) => button.addEventListener("click", findInput));
+
+  function findInput(event) {
+    switch (event.target.id) {
+      case "daysBtn":
+        value = days.value;
+        type = "days";
+        break;
+      case "hoursBtn":
+        value = hours.value;
+        type = "hours";
+        break;
+      case "minutesBtn":
+        value = minutes.value;
+        type = "minutes";
+        break;
+      case "secondsBtn":
+        value = seconds.value;
+        type = "seconds";
+        break;
+    }
+    convert();
+  }
+
+  function convert() {
+    switch (type) {
+      case "days":
+        hours.value = value * 24;
+        minutes.value = value * 1440;
+        seconds.value = value * 86400;
+        break;
+      case "hours":
+        days.value = value / 24;
+        minutes.value = value * 60;
+        seconds.value = value * 3600;
+        break;
+      case "minutes":
+        days.value = value / 1440;
+        hours.value = value / 60;
+        seconds.value = value * 60;
+        break;
+      case "seconds":
+        days.value = value / 86400;
+        hours.value = value / 3600;
+        minutes.value = value / 60;
+        break;
+    }
+  }
 }
