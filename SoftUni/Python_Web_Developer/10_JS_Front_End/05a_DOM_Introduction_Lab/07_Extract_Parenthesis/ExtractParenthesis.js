3c17d0441dd0065cf089d5e4c4ed66840f4bae09
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-29 19:02:28 +0200

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/05a_DOM_Introduction_Lab/07_Extract_Parenthesis/ExtractParenthesis.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/05a_DOM_Introduction_Lab/07_Extract_Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,10 @@
 function extract(content) {
+  content = document.getElementById("content").innerHTML;
+  let list = [];
+  var textSplit = content.split("(");
+  for (let i = 1; i < textSplit.length; i++) {
+    list.push(textSplit[i].split(")")[0]);
+  }
 
+  return list.join(", ");
 }
