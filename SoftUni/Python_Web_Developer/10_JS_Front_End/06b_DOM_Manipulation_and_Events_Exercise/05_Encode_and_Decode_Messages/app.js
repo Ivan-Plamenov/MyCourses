360bd47ef703d7d8dbfd9b0c167de69f4e6569d6
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-29 19:05:50 +0200

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/06b_DOM_Manipulation_and_Events_Exercise/05_Encode_and_Decode_Messages/app.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/06b_DOM_Manipulation_and_Events_Exercise/05_Encode_and_Decode_Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  var encodeBox = document.querySelectorAll("textarea")[0];
+  var encodeBtn = document.querySelectorAll("button")[0];
+  var decodeBox = document.querySelectorAll("textarea")[1];
+  var decodeBtn = document.querySelectorAll("button")[1];
+
+  encodeBtn.addEventListener("click", encode);
+  decodeBtn.addEventListener("click", decode);
+
+  function encode() {
+    let message = encodeBox.value;
+    let newMsg = "";
+    for (let i = 0; i < message.length; i++) {
+      newMsg += String.fromCodePoint(message[i].charCodeAt(0) + 1);
+    }
+    decodeBox.value = newMsg;
+    encodeBox.value = "";
+  }
+
+  function decode() {
+    let message = decodeBox.value;
+    let newMsg = "";
+    for (let i = 0; i < message.length; i++) {
+      newMsg += String.fromCodePoint(message[i].charCodeAt(0) - 1);
+    }
+    decodeBox.value = newMsg;
+  }
 }
