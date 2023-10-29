bd92574f71d79fa0b18e207ab9c746dec410d859
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-29 19:44:24 +0200

    Solution Updated*

--- SoftUni/JS_Web_Developer/04_JS_Applications/01_Asynchronous_Programming_Exercise/06_Blog/app.js
+++ SoftUni/JS_Web_Developer/04_JS_Applications/01_Asynchronous_Programming_Exercise/06_Blog/app.js
@@ -1,73 +1,57 @@
-const cache = { posts: null, comments: null };
-const url = "http://localhost:3030/jsonstore/blog/";
-
-function createOption({ id, title }) {
-  const e = document.createElement("option");
-  e.textContent = title;
-  e.id = id;
-
-  return e;
-}
-
-const clearOutput = (...arr) => arr.forEach((x) => (x.innerHTML = ""));
-
-async function getData(uri) {
-  const data = await fetch(`${url}${uri}`);
-
-  return await data.json();
-}
-
-async function loadData(type) {
-  if (cache[type] === null) {
-    const data = await getData(type);
-    console.log(data);
-    cache[type] = data;
-  }
-}
-
-async function displayPosts() {
-  await loadData("posts");
-  const selectElement = document.getElementById(`posts`);
-  selectElement.innerHTML = "";
-
-  Object.values(cache.posts).forEach((x) =>
-    selectElement.appendChild(createOption(x))
-  );
-}
-
-async function displayPost() {
-  await loadData("comments");
-  const html = {
-    postTitle: document.getElementById(`post-title`),
-    postBody: document.getElementById(`post-body`),
-    postComments: document.getElementById(`post-comments`),
-    selectElement: document.getElementById(`posts`),
-  };
-  const selected = html.selectElement.options[html.selectElement.selectedIndex];
-  const comments = Object.values(cache.comments).filter(
-    (x) => x.postId === selected.id
-  );
-
-  clearOutput(html.postTitle, html.postBody, html.postComments);
-
-  html.postTitle.textContent = selected.value;
-  html.postBody.textContent = cache.posts[selected.id].body;
-  html.postComments.innerHTML = comments
-    .map((x) => `<li id=${x.id}>${x.text}</li>`)
-    .join("");
-}
-
 function attachEvents() {
-  document.addEventListener("click", (e) => {
-    if (e.target.tagName === "BUTTON") {
-      const btns = {
-        btnViewPost: displayPost,
-        btnLoadPosts: displayPosts,
-      };
+  const baseUrl = `http://localhost:3030`;
+  const postsEndpoint = `${baseUrl}/jsonstore/blog/posts`;
+  const commentsEndpoint = `${baseUrl}/jsonstore/blog/comments`;
 
-      btns[e.target.id]();
+  const dropdownPostsMenu = document.getElementById("posts");
+
+  const postTitle = document.getElementById("post-title");
+  const postBody = document.getElementById("post-body");
+  const postComments = document.getElementById("post-comments");
+
+  const buttonLoadPosts = document.getElementById("btnLoadPosts");
+  buttonLoadPosts.addEventListener("click", loadPosts);
+
+  const buttonViewPost = document.getElementById("btnViewPost");
+  buttonViewPost.addEventListener("click", viewPost);
+
+  async function viewPost() {
+    postComments.innerHTML = "";
+
+    const postsResponse = await fetch(postsEndpoint);
+    const postsResult = await postsResponse.json();
+
+    postTitle.textContent =
+      postsResult[dropdownPostsMenu.value].title.toUpperCase();
+    postBody.textContent = postsResult[dropdownPostsMenu.value].body;
+
+    const commentsResponse = await fetch(commentsEndpoint);
+    const commentsResult = await commentsResponse.json();
+
+    let asArr = Object.entries(commentsResult);
+    asArr = asArr.filter(
+      ([, value]) => value.postId === dropdownPostsMenu.value
+    );
+
+    for (const [key, value] of asArr) {
+      const liElement = document.createElement("li");
+      liElement.id = key;
+      liElement.textContent = value.text;
+      postComments.appendChild(liElement);
     }
-  });
+  }
+
+  async function loadPosts() {
+    const postsResponse = await fetch(postsEndpoint);
+    const postsResult = await postsResponse.json();
+
+    for (const key in postsResult) {
+      const optionElement = document.createElement("option");
+      optionElement.textContent = postsResult[key].title;
+      optionElement.value = key;
+      dropdownPostsMenu.appendChild(optionElement);
+    }
+  }
 }
 
 attachEvents();
