830e5e97ff0696ebcc6eda19f62c3bb54a527385
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-29 19:08:52 +0200

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/06b_DOM_Manipulation_and_Events_Exercise/07_Distance_Converter/distanceConverter.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/06b_DOM_Manipulation_and_Events_Exercise/07_Distance_Converter/distanceConverter.js
@@ -1,3 +1,35 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+  var button = document.getElementById("convert");
+  var input = document.getElementById("inputDistance");
+  var output = document.getElementById("outputDistance");
+
+  button.addEventListener("click", convert);
+
+  function convert() {
+    let selected = input.parentNode.children[2].value;
+    let targeted = output.parentNode.children[2].value;
+    let initial = 0;
+    let final = 0;
+    var measures = [
+      ["km", 1000],
+      ["m", 1],
+      ["cm", 0.01],
+      ["mm", 0.001],
+      ["mi", 1609.34],
+      ["yrd", 0.9144],
+      ["ft", 0.3048],
+      ["in", 0.0254],
+    ];
+    for (let measure of measures) {
+      if (measure[0] === selected) {
+        initial = input.value * measure[1];
+      }
+    }
+    for (let measure of measures) {
+      if (measure[0] === targeted) {
+        final = initial / measure[1];
+      }
+    }
+    output.value = final;
+  }
 }
