8a9aadbb0777728188be0a5d43887aab1da7d3e2
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-10-29 19:03:29 +0200

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/05b_DOM_Introduction_Exercise/09_Number_Convertor/solution.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/05b_DOM_Introduction_Exercise/09_Number_Convertor/solution.js
@@ -1,4 +1,31 @@
 function solve() {
+  // Create buttons and remove empty one
+  const target = document.getElementById("selectMenuTo");
 
-    //TODO...
+  const binary = document.createElement("option");
+  const hex = document.createElement("option");
+
+  binary.textContent = "Binary";
+  binary.value = "binary";
+
+  hex.textContent = "Hexadecimal";
+  hex.value = "hexadecimal";
+
+  target.appendChild(binary);
+  target.appendChild(hex);
+
+  // Convert value
+
+  const button = document.querySelector("#container > button");
+
+  const selectMap = {
+    binary: (num) => num.toString(2),
+    hexadecimal: (num) => num.toString(16).toUpperCase(),
+  };
+
+  button.addEventListener("click", (event) => {
+    const input = document.getElementById("input");
+    const output = document.getElementById("result");
+    output.value = selectMap[target.value](+input.value);
+  });
 }
