4c326e7f35966ac776e000ad0b5b5b35d41d8f58
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-11-11 12:22:06 +0200

    Solution Added

--- SoftUni/Python_Web_Developer/04_Python_OOP/00a_Exam_Preparation/2020.08.16/03_Unit_Testing/test/test_paint_factory.py
+++ SoftUni/Python_Web_Developer/04_Python_OOP/00a_Exam_Preparation/2020.08.16/03_Unit_Testing/test/test_paint_factory.py
@@ -1,39 +1,49 @@
-# 71/100
 import unittest
 from project.factory.paint_factory import PaintFactory
 
 
 class TestPaintFactory(unittest.TestCase):
     def setUp(self):
-        # Initialize a PaintFactory object for testing
-        self.paint_factory = PaintFactory("Test Paint Factory", capacity=500)
-
-    def tearDown(self):
-        # Clean up resources used for testing (if necessary)
-        pass
+        self.factory = PaintFactory("TestFactory", 100)
 
     def test_constructor(self):
-        # Test the constructor of the PaintFactory class
-        self.assertEqual(self.paint_factory.name, "Test Paint Factory")
-        self.assertEqual(self.paint_factory.capacity, 500)
-        self.assertEqual(self.paint_factory.ingredients, {})
-        self.assertEqual(
-            self.paint_factory.valid_ingredients,
-            ["white", "yellow", "blue", "green", "red"],
+        self.assertEqual(self.factory.name, "TestFactory")
+        self.assertEqual(self.factory.capacity, 100)
+        self.assertListEqual(
+            self.factory.valid_ingredients, ["white", "yellow", "blue", "green", "red"]
         )
+        self.assertEqual(self.factory.ingredients, {})
 
-    def test_add_ingredient(self):
-        # Test the add_ingredient method
-        self.paint_factory.add_ingredient("white", 50)
-        self.assertEqual(self.paint_factory.ingredients["white"], 50)
+    def test_add_ingredient_valid(self):
+        self.factory.add_ingredient("red", 20)
+        self.assertIn("red", self.factory.ingredients)
+        self.assertEqual(self.factory.ingredients["red"], 20)
 
+    def test_add_ingredient_exceeds_capacity(self):
         with self.assertRaises(ValueError):
-            self.paint_factory.add_ingredient("blue", 600)  # Exceeds capacity
+            self.factory.add_ingredient("blue", 101)
 
+    def test_add_ingredient_invalid(self):
         with self.assertRaises(TypeError):
-            self.paint_factory.add_ingredient("black", 20)  # Invalid ingredient
+            self.factory.add_ingredient("purple", 10)
 
-    # Define more test methods for other functions of PaintFactory
+    def test_remove_ingredient_valid(self):
+        self.factory.add_ingredient("yellow", 30)
+        self.factory.remove_ingredient("yellow", 10)
+        self.assertEqual(self.factory.ingredients["yellow"], 20)
+
+    def test_remove_ingredient_not_exists(self):
+        with self.assertRaises(KeyError):
+            self.factory.remove_ingredient("orange", 10)
+
+    def test_remove_ingredient_excess(self):
+        self.factory.add_ingredient("green", 25)
+        with self.assertRaises(ValueError):
+            self.factory.remove_ingredient("green", 30)
+
+    def test_products_property(self):
+        self.factory.add_ingredient("white", 50)
+        self.assertDictEqual(self.factory.products, {"white": 50})
 
 
 if __name__ == "__main__":
