bf03ed2a8d15945c0e7496321dbcb3cbe6f6982e
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-11-13 10:37:05 +0200

    Solution Added

--- SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/2023.04.20_Retake/01_Horse_Racing/01_Horse_Racing.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/2023.04.20_Retake/01_Horse_Racing/01_Horse_Racing.js
@@ -1,3 +1,57 @@
-function horseRaceSimulation(input) {
-  // TO DO
+function horseRace(input) {
+  let horses = input.shift().split("|");
+
+  for (let command of input) {
+    if (command === "Finish") {
+      break;
+    }
+
+    let [action, ...args] = command.split(" ");
+
+    if (action === "Retake") {
+      let overtakingHorse = args[0];
+      let overtakenHorse = args[1];
+      let overtakingIndex = horses.indexOf(overtakingHorse);
+      let overtakenIndex = horses.indexOf(overtakenHorse);
+
+      if (
+        overtakingIndex !== -1 &&
+        overtakenIndex !== -1 &&
+        overtakingIndex < overtakenIndex
+      ) {
+        [horses[overtakingIndex], horses[overtakenIndex]] = [
+          horses[overtakenIndex],
+          horses[overtakingIndex],
+        ];
+        console.log(`${overtakingHorse} retakes ${overtakenHorse}.`);
+      }
+    } else if (action === "Trouble") {
+      let horseName = args[0];
+      let horseIndex = horses.indexOf(horseName);
+
+      if (horseIndex !== -1 && horseIndex < horses.length - 1) {
+        [horses[horseIndex], horses[horseIndex + 1]] = [
+          horses[horseIndex + 1],
+          horses[horseIndex],
+        ];
+        console.log(`Trouble for ${horseName} - drops one position.`);
+      }
+    } else if (action === "Rage") {
+      let horseName = args[0];
+      let horseIndex = horses.indexOf(horseName);
+
+      if (horseIndex === 1) {
+        [horses[0], horses[1]] = [horses[1], horses[0]];
+      }
+
+      console.log(`${horseName} rages 2 positions ahead.`);
+    } else if (action === "Miracle") {
+      let lastHorse = horses.pop();
+      horses.unshift(lastHorse);
+      console.log(`What a miracle - ${lastHorse} becomes first.`);
+    }
+  }
+
+  console.log(`The winner is: ${horses[0]}`);
+  console.log(horses.join("->"));
 }
