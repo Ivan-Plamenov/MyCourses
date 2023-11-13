ce400d16b903abf6c64e4ec2d564484fafaaa3f8
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-11-13 11:15:51 +0200

    Solution Updated

--- SoftUni/Python_Web_Developer/04_Python_OOP/00a_Exam_Preparation/2020.08.16/03_Unit_Testing/test/test_paint_factory.py
+++ SoftUni/Python_Web_Developer/04_Python_OOP/00a_Exam_Preparation/2020.08.16/03_Unit_Testing/test/test_paint_factory.py
@@ -6,44 +6,40 @@ class TestPaintFactory(unittest.TestCase):
     def setUp(self):
         self.factory = PaintFactory("TestFactory", 100)
 
-    def test_constructor(self):
+    def test_initialization(self):
         self.assertEqual(self.factory.name, "TestFactory")
         self.assertEqual(self.factory.capacity, 100)
-        self.assertListEqual(
-            self.factory.valid_ingredients, ["white", "yellow", "blue", "green", "red"]
-        )
         self.assertEqual(self.factory.ingredients, {})
 
-    def test_add_ingredient_valid(self):
+    def test_add_valid_ingredient_within_capacity(self):
         self.factory.add_ingredient("red", 20)
-        self.assertIn("red", self.factory.ingredients)
         self.assertEqual(self.factory.ingredients["red"], 20)
 
-    def test_add_ingredient_exceeds_capacity(self):
+    def test_add_valid_ingredient_exceeding_capacity(self):
         with self.assertRaises(ValueError):
             self.factory.add_ingredient("blue", 101)
 
-    def test_add_ingredient_invalid(self):
+    def test_add_invalid_ingredient(self):
         with self.assertRaises(TypeError):
-            self.factory.add_ingredient("purple", 10)
+            self.factory.add_ingredient("purple", 20)
 
-    def test_remove_ingredient_valid(self):
+    def test_remove_existing_ingredient_within_quantity(self):
         self.factory.add_ingredient("yellow", 30)
         self.factory.remove_ingredient("yellow", 10)
         self.assertEqual(self.factory.ingredients["yellow"], 20)
 
-    def test_remove_ingredient_not_exists(self):
+    def test_remove_ingredient_exceeding_quantity(self):
+        self.factory.add_ingredient("green", 15)
+        with self.assertRaises(ValueError):
+            self.factory.remove_ingredient("green", 20)
+
+    def test_remove_non_existing_ingredient(self):
         with self.assertRaises(KeyError):
             self.factory.remove_ingredient("orange", 10)
 
-    def test_remove_ingredient_excess(self):
-        self.factory.add_ingredient("green", 25)
-        with self.assertRaises(ValueError):
-            self.factory.remove_ingredient("green", 30)
-
     def test_products_property(self):
         self.factory.add_ingredient("white", 50)
-        self.assertDictEqual(self.factory.products, {"white": 50})
+        self.assertEqual(self.factory.products, {"white": 50})
 
 
 if __name__ == "__main__":
