3ca92f80d5a3c057c1f81e82bee31bccda9adf22
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-11-13 10:42:19 +0200

    Solution Updated

--- SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/2023.08.17_Retake/01_Astro_Adventure/01_Astro_Adventure.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/2023.08.17_Retake/01_Astro_Adventure/01_Astro_Adventure.js
@@ -1,50 +1,52 @@
 function spaceMission(input) {
-  let numAstronauts = parseInt(input[0], 10);
-  let astronauts = {};
+  let n = Number(input.shift());
+  let astronauts = [];
 
-  // Parse astronauts' details
-  for (let i = 1; i <= numAstronauts; i++) {
-    let [name, oxygen, energy] = input[i]
-      .split(" ")
-      .map((el, idx) => (idx > 0 ? parseInt(el, 10) : el));
-    astronauts[name] = { oxygen, energy };
+  // Populate initial astronaut data
+  for (let i = 0; i < n; i++) {
+    let [name, oxygen, energy] = input[i].split(" ");
+    oxygen = Number(oxygen);
+    energy = Number(energy);
+    astronauts.push({ name, oxygen, energy });
   }
 
   // Process commands
-  for (let i = numAstronauts + 1; i < input.length; i++) {
-    let [command, name, amount] = input[i].split(" - ");
-    amount = parseInt(amount, 10);
-
-    switch (command) {
-      case "Explore":
-        if (astronauts[name].energy >= amount) {
-          astronauts[name].energy -= amount;
-          console.log(
-            `${name} has successfully explored a new area and now has ${astronauts[name].energy} energy!`
-          );
-        } else {
-          console.log(`${name} does not have enough energy to explore!`);
-        }
-        break;
-      case "Refuel":
-        let energyToAdd = Math.min(200 - astronauts[name].energy, amount);
-        astronauts[name].energy += energyToAdd;
-        console.log(`${name} refueled their energy by ${energyToAdd}!`);
-        break;
-      case "Breathe":
-        let oxygenToAdd = Math.min(100 - astronauts[name].oxygen, amount);
-        astronauts[name].oxygen += oxygenToAdd;
-        console.log(
-          `${name} took a breath and recovered ${oxygenToAdd} oxygen!`
-        );
-        break;
-      case "End":
-        Object.keys(astronauts).forEach((astronaut) => {
-          console.log(
-            `Astronaut: ${astronaut}, Oxygen: ${astronauts[astronaut].oxygen}, Energy: ${astronauts[astronaut].energy}`
-          );
-        });
-        return; // End the function as the mission is over
+  for (let command of input) {
+    if (command === "End") {
+      break;
     }
+
+    let [action, astronautName, amount] = command.split(" - ");
+    amount = Number(amount);
+
+    let astronaut = astronauts.find((a) => a.name === astronautName);
+
+    if (action === "Explore") {
+      if (astronaut.energy >= amount) {
+        astronaut.energy -= amount;
+        console.log(
+          `${astronautName} has successfully explored a new area and now has ${astronaut.energy} energy!`
+        );
+      } else {
+        console.log(`${astronautName} does not have enough energy to explore!`);
+      }
+    } else if (action === "Refuel") {
+      let recovered = Math.min(200 - astronaut.energy, amount);
+      astronaut.energy += recovered;
+      console.log(`${astronautName} refueled their energy by ${recovered}!`);
+    } else if (action === "Breathe") {
+      let recovered = Math.min(100 - astronaut.oxygen, amount);
+      astronaut.oxygen += recovered;
+      console.log(
+        `${astronautName} took a breath and recovered ${recovered} oxygen!`
+      );
+    }
+  }
+
+  // Print astronaut data
+  for (let astronaut of astronauts) {
+    console.log(
+      `Astronaut: ${astronaut.name}, Oxygen: ${astronaut.oxygen}, Energy: ${astronaut.energy}`
+    );
   }
 }
