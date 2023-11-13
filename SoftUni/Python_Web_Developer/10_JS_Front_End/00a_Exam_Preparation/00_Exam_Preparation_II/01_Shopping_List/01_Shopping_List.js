51c9624b86514936e9d92979f90e6a15cdca7908
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-11-13 10:30:58 +0200

    Solution Updated

--- SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/00_Exam_Preparation_II/01_Shopping_List/01_Shopping_List.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/00_Exam_Preparation_II/01_Shopping_List/01_Shopping_List.js
@@ -1,44 +1,35 @@
 function groceryList(input) {
-  const groceries = input.shift().split("!");
+  let groceries = input.shift().split("!");
 
-  for (const command of input) {
-    const [action, ...args] = command.split(" ");
+  for (let command of input) {
+    let [action, ...args] = command.split(" ");
+    let item = args.join(" ");
 
-    switch (action) {
-      case "Urgent":
-        const newItem = args[0];
-        if (!groceries.includes(newItem)) {
-          groceries.unshift(newItem);
-        }
-        break;
-      case "Unnecessary":
-        const itemToRemove = args[0];
-        const indexToRemove = groceries.indexOf(itemToRemove);
-        if (indexToRemove !== -1) {
-          groceries.splice(indexToRemove, 1);
-        }
-        break;
-      case "Correct":
-        const oldItem = args[0];
-        const newItemName = args[1];
-        const indexToCorrect = groceries.indexOf(oldItem);
-        if (indexToCorrect !== -1) {
-          groceries[indexToCorrect] = newItemName;
-        }
-        break;
-      case "Rearrange":
-        const itemToMove = args[0];
-        const indexToMove = groceries.indexOf(itemToMove);
-        if (indexToMove !== -1) {
-          groceries.splice(indexToMove, 1);
-          groceries.push(itemToMove);
-        }
-        break;
-      case "Go":
-        if (args[0] === "Shopping!") {
-          return groceries.join(", ");
-        }
-        break;
+    if (action === "Urgent") {
+      if (!groceries.includes(item)) {
+        groceries.unshift(item);
+      }
+    } else if (action === "Unnecessary") {
+      let index = groceries.indexOf(item);
+      if (index !== -1) {
+        groceries.splice(index, 1);
+      }
+    } else if (action === "Correct") {
+      let [oldItem, newItem] = args;
+      let index = groceries.indexOf(oldItem);
+      if (index !== -1) {
+        groceries[index] = newItem;
+      }
+    } else if (action === "Rearrange") {
+      let index = groceries.indexOf(item);
+      if (index !== -1) {
+        groceries.splice(index, 1);
+        groceries.push(item);
+      }
+    } else if (action === "Go") {
+      break;
     }
   }
+
+  console.log(groceries.join(", "));
 }
