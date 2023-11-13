6c8483a27bd508712583e4107c0dd8ea57b07e1a
Ivan Plamenov <144794868+Ivan-Plamenov@users.noreply.github.com>
2023-11-13 10:40:30 +0200

    Solution Updated

--- SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/2023.08.05/01_MotoGP_Race/01_MotoGP_Race.js
+++ SoftUni/Python_Web_Developer/10_JS_Front_End/00a_Exam_Preparation/2023.08.05/01_MotoGP_Race/01_MotoGP_Race.js
@@ -1,63 +1,70 @@
 // 90 / 100
 
-function motoGPRace(inputCommands) {
-  let numRiders = parseInt(inputCommands[0], 10);
-  let riders = {};
+function motoGPRace(input) {
+  let n = Number(input.shift());
+  let riders = [];
 
-  // Initialize riders' details
-  for (let i = 1; i <= numRiders; i++) {
-    let [rider, fuel, position] = inputCommands[i]
-      .split("|")
-      .map((x, idx) => (idx === 0 ? x : parseInt(x, 10)));
-    riders[rider] = { fuel, position };
+  // Populate initial rider data
+  for (let i = 0; i < n; i++) {
+    let [rider, fuel, position] = input[i].split("|");
+    fuel = Number(fuel);
+    position = Number(position);
+    riders.push({ rider, fuel, position });
   }
 
   // Process commands
-  for (let command of inputCommands.slice(numRiders + 1)) {
-    if (command === "Finish") {
+  for (let command of input) {
+    let [action, ...args] = command.split(" - ");
+
+    if (action === "Finish") {
       break;
     }
 
-    let parts = command.split(" - ");
-    let action = parts[0];
-
     if (action === "StopForFuel") {
-      let [rider, minFuel, changedPosition] = parts
-        .slice(1)
-        .map((x, idx) => (idx === 0 ? x : parseInt(x, 10)));
-      if (riders[rider].fuel < minFuel) {
-        console.log(
-          `${rider} stopped to refuel but lost his position, now he is ${changedPosition}.`
-        );
-        riders[rider].position = changedPosition;
-      } else {
-        console.log(`${rider} does not need to stop for fuel!`);
+      let riderName = args[0];
+      let minimumFuel = Number(args[1]);
+      let changedPosition = Number(args[2]);
+
+      let rider = riders.find((r) => r.rider === riderName);
+      if (rider) {
+        if (rider.fuel < minimumFuel) {
+          rider.position = changedPosition;
+          console.log(
+            `${rider.rider} stopped to refuel but lost his position, now he is ${changedPosition}.`
+          );
+        } else {
+          console.log(`${rider.rider} does not need to stop for fuel!`);
+        }
       }
     } else if (action === "Overtaking") {
-      let [rider1, rider2] = parts.slice(1);
-      if (riders[rider1].position < riders[rider2].position) {
-        [riders[rider1].position, riders[rider2].position] = [
-          riders[rider2].position,
-          riders[rider1].position,
-        ];
-        console.log(`${rider1} overtook ${rider2}!`);
+      let rider1Name = args[0];
+      let rider2Name = args[1];
+
+      let rider1 = riders.find((r) => r.rider === rider1Name);
+      let rider2 = riders.find((r) => r.rider === rider2Name);
+
+      if (rider1 && rider2 && rider1.position < rider2.position) {
+        [rider1.position, rider2.position] = [rider2.position, rider1.position];
+        console.log(`${rider1.rider} overtook ${rider2.rider}!`);
       }
     } else if (action === "EngineFail") {
-      let [rider, lapsLeft] = parts
-        .slice(1)
-        .map((x, idx) => (idx === 0 ? x : parseInt(x, 10)));
-      console.log(
-        `${rider} is out of the race because of a technical issue, ${lapsLeft} laps before the finish.`
-      );
-      delete riders[rider];
+      let riderName = args[0];
+      let lapsLeft = Number(args[1]);
+
+      let riderIndex = riders.findIndex((r) => r.rider === riderName);
+      if (riderIndex !== -1) {
+        riders.splice(riderIndex, 1);
+        console.log(
+          `${riderName} is out of the race because of a technical issue, ${lapsLeft} laps before the finish.`
+        );
+      }
     }
   }
 
-  // Sort and print final positions
-  let finalPositions = Object.keys(riders).sort(
-    (a, b) => riders[a].position - riders[b].position
-  );
-  finalPositions.forEach((rider) => {
-    console.log(`${rider}\n  Final position: ${riders[rider].position}`);
-  });
+  // Sort riders by position and print the final positions
+  riders.sort((a, b) => a.position - b.position);
+
+  for (let rider of riders) {
+    console.log(`${rider.rider}\n  Final position: ${rider.position}`);
+  }
 }
